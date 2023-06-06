3b34bd50fa37d3f067614547cb676010b9549cff
khs960616 <anjfkrhdkdle@naver.com>
2023-06-07 02:29:59 +0900

    feat: post 연관 엔티티 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/domain/PostLike.java
@@ -0,0 +1,31 @@
+package com.ssafy.ssafsound.domain.post.domain;
+
+import com.ssafy.ssafsound.domain.BaseTimeEntity;
+import com.ssafy.ssafsound.domain.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name="post_like")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostLike extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "post_like_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
