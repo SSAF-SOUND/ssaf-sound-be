90c65fb09a7a1b5dadc2fff5f9e5401edefc69ad
khs960616 <anjfkrhdkdle@naver.com>
2023-06-07 02:32:51 +0900

    feat: 점심메뉴 연관 엔티티 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/domain/Lunch.java
@@ -0,0 +1,39 @@
+package com.ssafy.ssafsound.domain.lunch.domain;
+
+import com.ssafy.ssafsound.domain.BaseTimeEntity;
+import com.ssafy.ssafsound.domain.meta.domain.Campus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name="lunch")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lunch extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "lunch_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String mainMenu;
+
+    @Column
+    private String extraMenu;
+
+    @Column
+    private String imagePath;
+
+    @Column
+    private String menuKcal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "campus_id")
+    private Campus campus;
+}
