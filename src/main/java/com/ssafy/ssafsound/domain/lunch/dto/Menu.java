19e561af85fb812e02a96d57d094deec223b9295
moonn6pence <shin421179@gmail.com>
2023-06-10 22:10:48 +0900

    feat: 메뉴 조회 api MVC구조 추가 - 컨트롤러는 Envelope 패턴 사용 - DTO 변환은 service에 위임

---
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/dto/Menu.java
@@ -0,0 +1,17 @@
+package com.ssafy.ssafsound.domain.lunch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Menu {
+
+    private String mainMenu;
+
+    private String imagePath;
+
+    private Long vote;
+
+
+}
