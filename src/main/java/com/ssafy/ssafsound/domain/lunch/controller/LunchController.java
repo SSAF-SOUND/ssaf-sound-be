38c1c68a1a15813e65e026e016e11e753b968885
moonn6pence <shin421179@gmail.com>
2023-06-14 23:00:48 +0900

    feat: 런치 조회 기능
    - service 레이어 stream으로 변경
    - 추후에 LunchRepository의 캠퍼스 부분은 Enum으로 수정될 때 같이 변경 필요
    - Controller 단에서 Validation 추가

--- src/main/java/com/ssafy/ssafsound/domain/lunch/controller/LunchController.java
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/controller/LunchController.java
@@ -1,14 +1,18 @@
 package com.ssafy.ssafsound.domain.lunch.controller;
 
-import com.ssafy.ssafsound.domain.lunch.dto.MenuDetail;
-import com.ssafy.ssafsound.domain.lunch.dto.Menus;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListReqDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListResDto;
 import com.ssafy.ssafsound.domain.lunch.service.LunchService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController("/lunch")
 @RequiredArgsConstructor
@@ -17,22 +21,22 @@ public class LunchController {
     private final LunchService lunchService;
 
     @GetMapping("/{lunchId}")
-    public EnvelopeResponse<MenuDetail> getLunchByLunchId(@PathVariable String lunchId){
+    public EnvelopeResponse<GetLunchResDto> getLunchByLunchId(@PathVariable @NumberFormat Long lunchId){
 
-        return EnvelopeResponse.<MenuDetail>builder()
+        return EnvelopeResponse.<GetLunchResDto>builder()
                 .code(HttpStatus.OK.toString())
                 .message("success")
-                .data(lunchService.findLunchByLunchId())
+                .data(lunchService.findLunchByLunchId(lunchId))
                 .build();
     }
 
     @GetMapping
-    public EnvelopeResponse<Menus> getLunchesByCampusAndDate(Integer campus, String date){
+    public EnvelopeResponse<GetLunchListResDto> getLunchesByCampusAndDate(@Valid GetLunchListReqDto getLunchListReqDto){
 
-        return EnvelopeResponse.<Menus>builder()
+        return EnvelopeResponse.<GetLunchListResDto>builder()
                 .code(HttpStatus.OK.toString())
                 .message("success")
-                .data(lunchService.findLunchesByCampusAndDate())
+                .data(lunchService.findLunchesByCampusAndDate(getLunchListReqDto))
                 .build();
     }
 }
--- src/main/java/com/ssafy/ssafsound/domain/lunch/service/LunchService.java
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/service/LunchService.java
@@ -1,16 +1,46 @@
 package com.ssafy.ssafsound.domain.lunch.service;
 
-import com.ssafy.ssafsound.domain.lunch.dto.MenuDetail;
-import com.ssafy.ssafsound.domain.lunch.dto.Menus;
+import com.ssafy.ssafsound.domain.lunch.domain.Lunch;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListElementResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListReqDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListResDto;
+import com.ssafy.ssafsound.domain.lunch.exception.LunchErrorInfo;
+import com.ssafy.ssafsound.domain.lunch.exception.LunchException;
+import com.ssafy.ssafsound.domain.lunch.repository.LunchPollRepository;
+import com.ssafy.ssafsound.domain.lunch.repository.LunchRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class LunchService {
-    public Menus findLunchesByCampusAndDate() {
-        return null;
+
+    private final LunchRepository lunchRepository;
+    private final LunchPollRepository lunchPollRepository;
+
+    public GetLunchListResDto findLunchesByCampusAndDate(GetLunchListReqDto getLunchListReqDto) {
+
+        Integer dayDifference = getLunchListReqDto.getDate().compareTo(LocalDate.now());
+        if (dayDifference < 0 || dayDifference > 1) throw new LunchException(LunchErrorInfo.INVALID_LUNCH_DATE);
+
+        List<Lunch> lunches = lunchRepository.findAllByCampus_IdAndDate(getLunchListReqDto.getCampusId(), getLunchListReqDto.getDate())
+                .orElseThrow(()->new LunchException(LunchErrorInfo.NO_LUNCH_DATE));
+
+        return GetLunchListResDto.of(lunches.stream()
+                .map(lunch -> GetLunchListElementResDto.of(lunch, lunchPollRepository.countAllByLunch(lunch)))
+                .collect(Collectors.toList()));
     }
 
-    public MenuDetail findLunchByLunchId() {
-        return null;
+    public GetLunchResDto findLunchByLunchId(Long lunchId) {
+
+        return GetLunchResDto.of(lunchRepository.findLunchById(lunchId)
+                .orElseThrow(()->new LunchException(LunchErrorInfo.INVALID_LUNCH_ID)));
     }
 }
