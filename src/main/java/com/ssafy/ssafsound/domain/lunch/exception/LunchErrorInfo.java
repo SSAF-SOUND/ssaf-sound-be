11c7a43f55502458ef65a183af7304ab8f61d279
moonn6pence <shin421179@gmail.com>
2023-06-14 22:57:57 +0900

    feat: 예외 처리 로직 추가
    - ControllerAdvice
        - 서버 내부 에러, 비즈니스 에러, 클라이언트 에러 구분
        - 각 도메인 별로 도메인 익셉션 클래스 만들고 추가 바람

---
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/exception/LunchErrorInfo.java
@@ -0,0 +1,18 @@
+package com.ssafy.ssafsound.domain.lunch.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum LunchErrorInfo {
+    INVALID_LUNCH_DATE("601","유효하지 않은 날짜입니다."),
+    INVALID_LUNCH_ID("602","유효하지 않은 점심 데이터입니다."),
+    NO_LUNCH_DATE("603","점심 데이터가 존재하지 않는 날짜입니다.");
+
+    private String code;
+    private String message;
+
+    LunchErrorInfo(String code, String message){
+        this.code = code;
+        this.message = message;
+    }
+}
--- src/main/java/com/ssafy/ssafsound/global/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/ssafy/ssafsound/global/advice/ExceptionControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.ssafy.ssafsound.global.advice;
 
+import com.ssafy.ssafsound.domain.lunch.exception.LunchErrorInfo;
+import com.ssafy.ssafsound.domain.lunch.exception.LunchException;
 import com.ssafy.ssafsound.global.common.exception.GlobalErrorInfo;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,8 +15,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
+    @ExceptionHandler(LunchException.class)
+    public EnvelopeResponse LunchExceptionHandler(LunchException e){
+        log.error(e.getMessage());
+
+        return EnvelopeResponse.builder()
+                .code(e.getInfo().getCode())
+                .message((e.getInfo().getMessage()))
+                .build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public EnvelopeResponse BadRequestExceptionHandler(ConstraintViolationException e){
+        log.error(e.getMessage());
+
+        return EnvelopeResponse.builder()
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .message(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .build();
+    }
+
     @ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.OK)
     public EnvelopeResponse RuntimeExceptionHandler(RuntimeException e) {
         log.error(e.getMessage());
         return EnvelopeResponse.builder()
