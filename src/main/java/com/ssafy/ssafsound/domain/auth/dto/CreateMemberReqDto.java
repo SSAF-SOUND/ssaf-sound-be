edb6a6fe638b65c0a984e6fa25e7848aa815d93d
김태용 <amico741@naver.com>
2023-06-15 11:07:54 +0900

    feat: login 시도 시 Request Validation 추가

--- src/main/java/com/ssafy/ssafsound/domain/auth/dto/CreateMemberReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/auth/dto/CreateMemberReqDto.java
@@ -7,12 +7,14 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 public class CreateMemberReqDto {
     @NotBlank(message = "코드가 유효하지 않습니다.")
     private String code;
 
+    @Pattern(regexp = "^(google|kakao|github|apple)$")
     @NotBlank(message = "Oauth name이 유효하지 않습니다.")
     private String oauthName;
 }
