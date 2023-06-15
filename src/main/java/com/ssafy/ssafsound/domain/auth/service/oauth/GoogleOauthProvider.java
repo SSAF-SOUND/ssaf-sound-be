17ded3254c9b403ae389b5e96f102bbb294727b6
김태용 <amico741@naver.com>
2023-06-16 02:01:39 +0900

    feat: OauthProvider 인터페이스 및 구글 Provider 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/auth/service/oauth/GoogleOauthProvider.java
@@ -0,0 +1,40 @@
+package com.ssafy.ssafsound.domain.auth.service.oauth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Component
+public class GoogleOauthProvider implements OauthProvider {
+
+    private final RestTemplate restTemplate;
+    @Value("${spring.oauth2.google.url}")
+    private String GOOGLE_URL;
+    @Value("spring.oauth2.google.token-url")
+    private String GOOGLE_TOKEN_URL;
+    @Value("${spring.oauth2.google.client-id}")
+    private String GOOGLE_CLIENT_ID;
+    @Value("${spring.oauth2.google.client-secret}")
+    private String GOOGLE_CLIENT_SECRET;
+    @Value("${spring.oauth2.google.client-url}")
+    private String GOOGLE_CLIENT_URL;
+    @Value("${spring.oauth2.google.scope}")
+    private String GOOGLE_DATA_ACCESS_SCOPE;
+
+    @Override
+    public String getOauthUrl() {
+        return null;
+    }
+
+    @Override
+    public String  getOauthAccessToken(String code) {
+        return null;
+    }
+
+    @Override
+    public String getUserOauthIdentifier(String accessToken) {
+        return null;
+    }
+}
