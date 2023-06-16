5041c6278e8b1c55eb6c1b5119b90c23f665407f
khs960616 <anjfkrhdkdle@naver.com>
2023-06-16 23:14:18 +0900

    feature: 등록자 리크루트 등록을 위한 DTO 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/recruit/dto/RecruitLimitElement.java
@@ -0,0 +1,11 @@
+package com.ssafy.ssafsound.domain.recruit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecruitLimitElement {
+    private String recruitType;
+    private int limit;
+}
