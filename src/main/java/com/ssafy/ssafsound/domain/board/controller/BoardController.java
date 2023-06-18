256aaa80b6572f37c824ce29d2c723953f3b56fe
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-06-19 01:57:19 +0900

    feat: 게시판 목록 조회 기능 구현
    * feat: 게시판 목록 조회 응답 DTO 구현
    
    * feat: 게시판 목록 조회 Service 구현
    
    * feat: 게시판 목록 조회 Controller 구현
    
    * fix: @Transactional 메소드에서 클래스 단위로 수정
    
    * fix: 초기 application-local.yml 누락 수정
    
    * fix: 트랜잭션 어노테이션 메소드 단위로 수정

---
+++ src/main/java/com/ssafy/ssafsound/domain/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.ssafy.ssafsound.domain.board.controller;
+
+import com.ssafy.ssafsound.domain.board.dto.GetBoardListResDto;
+import com.ssafy.ssafsound.domain.board.service.BoardService;
+import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/boards")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping
+    public EnvelopeResponse<GetBoardListResDto> findBoards() {
+        return EnvelopeResponse.<GetBoardListResDto>builder()
+                .data(boardService.findBoards())
+                .build();
+    }
+}
