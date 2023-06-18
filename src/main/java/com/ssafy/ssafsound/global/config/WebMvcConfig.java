237ae1475844e68470fe94972be0e6f13ef5224f
김태용 <amico741@naver.com>
2023-06-19 04:04:16 +0900

    feat: Spring Boot MVC Config 추가

---
+++ src/main/java/com/ssafy/ssafsound/global/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.ssafsound.global.config;
+
+import com.ssafy.ssafsound.global.interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public WebMvcConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/callback")
+                .excludePathPatterns("/boards");
+    }
+}
