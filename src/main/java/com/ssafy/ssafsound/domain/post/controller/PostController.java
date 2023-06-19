9be1c6192af9e43f3351590bf14810de9f8ec4be
jjuny0310 <jjuny7712@gmail.com>
2023-06-19 22:52:57 +0900

    feat: 게시글 목록 조회 컨트롤러 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.ssafy.ssafsound.domain.post.controller;
+
+import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
+import com.ssafy.ssafsound.domain.post.exception.PostErrorInfo;
+import com.ssafy.ssafsound.domain.post.exception.PostException;
+import com.ssafy.ssafsound.domain.post.service.PostService;
+import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public EnvelopeResponse<GetPostListResDto> findPosts(@RequestParam Long boardId, Pageable pageable) {
+        return EnvelopeResponse.<GetPostListResDto>builder()
+                .data(postService.findPosts(boardId, pageable))
+                .build();
+    }
+}
