7672a37017fcd22f57eb8387ac8e2eeca17202b2
김태용 <amico741@naver.com>
2023-06-19 20:36:56 +0900

    feat: WebMvcConfig에서 Resolver 등록

--- src/main/java/com/ssafy/ssafsound/global/config/WebMvcConfig.java
+++ src/main/java/com/ssafy/ssafsound/global/config/WebMvcConfig.java
@@ -1,18 +1,23 @@
 package com.ssafy.ssafsound.global.config;
 
+import com.ssafy.ssafsound.domain.auth.validator.AuthenticationArgumentResolver;
 import com.ssafy.ssafsound.global.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
 
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final AuthenticationArgumentResolver authenticationArgumentResolver;
 
-    public WebMvcConfig(AuthInterceptor authInterceptor) {
+    public WebMvcConfig(AuthInterceptor authInterceptor, AuthenticationArgumentResolver authenticationArgumentResolver) {
         this.authInterceptor = authInterceptor;
+        this.authenticationArgumentResolver = authenticationArgumentResolver;
     }
 
     @Override
@@ -21,5 +26,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/auth/callback")
                 .excludePathPatterns("/boards");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationArgumentResolver);
     }
 }
