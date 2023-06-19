ee7447347316f5076c2c81e5d4a64ead2a21dd1b
김태용 <amico741@naver.com>
2023-06-19 20:08:09 +0900

    feat: AuthenticatedMember를 얻기 위한 리졸버 클래스 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/auth/validator/AuthenticationArgumentResolver.java
@@ -0,0 +1,20 @@
+package com.ssafy.ssafsound.domain.auth.validator;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+    
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+}
