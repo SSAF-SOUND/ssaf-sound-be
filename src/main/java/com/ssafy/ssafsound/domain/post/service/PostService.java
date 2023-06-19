daaae1c77c3de5e4451b4c9e453a4fb5c86d30cb
jjuny0310 <jjuny7712@gmail.com>
2023-06-19 22:52:22 +0900

    feat: 게시글 목록 조회 Service 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
@@ -0,0 +1,27 @@
+package com.ssafy.ssafsound.domain.post.service;
+
+import com.ssafy.ssafsound.domain.board.domain.Board;
+import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
+import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
+import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
+import com.ssafy.ssafsound.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    public GetPostListResDto findPosts(Long boardId, Pageable pageable) {
+        return new GetPostListResDto(postRepository.findAllByBoardId(boardId, pageable)
+                .stream()
+                .map(GetPostResDto::from)
+                .collect(Collectors.toList()));
+    }
+}
