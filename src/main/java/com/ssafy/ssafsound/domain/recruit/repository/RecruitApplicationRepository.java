fd7a3e8b5d10fbe0c85037815f9a35155167fa31
khs960616 <anjfkrhdkdle@naver.com>
2023-06-19 22:37:58 +0900

    fix: Recruit Application 조회 쿼리 오류 수정

--- src/main/java/com/ssafy/ssafsound/domain/recruit/repository/RecruitApplicationRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruit/repository/RecruitApplicationRepository.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 
 @Repository
 public interface RecruitApplicationRepository extends JpaRepository<RecruitApplication, Long> {
-    @Query("SELECT r FROM recruit_application r left join fetch r.member")
-    Optional<RecruitApplication> findByIdFetchJoinMember(Long recruitApplicationId);
-
-    @Query("SELECT r FROM recruit_application r left join fetch r.recruit left join r.recruit.member")
+    @Query("SELECT r FROM recruit_application r left join fetch r.recruit left join r.recruit.member where r.id = :recruitApplicationId")
     Optional<RecruitApplication> findByIdFetchRecruitWriter(Long recruitApplicationId);
+
+    @Query("SELECT r FROM recruit_application r left join fetch r.recruit left join r.recruit.member where r.id = :recruitApplicationId and r.member.id = :memberId")
+    Optional<RecruitApplication> findByIdAndMemberIdFetchRecruitWriter(Long recruitApplicationId, Long memberId);
+
+    Optional<RecruitApplication> findByIdAndMemberId(Long recruitApplicationId, Long memberId);
 }
