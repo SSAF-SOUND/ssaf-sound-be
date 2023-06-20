f80bf03faa237b860872bdfed9967637ab2714bc
jjuny0310 <jjuny7712@gmail.com>
2023-06-21 02:10:59 +0900

    feat: 게시글 스크랩 Service 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
@@ -3,9 +3,12 @@ package com.ssafy.ssafsound.domain.post.service;
 import com.ssafy.ssafsound.domain.board.exception.BoardErrorInfo;
 import com.ssafy.ssafsound.domain.board.exception.BoardException;
 import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
+import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
+import com.ssafy.ssafsound.domain.post.domain.PostScrap;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
 import com.ssafy.ssafsound.domain.post.repository.PostRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,8 +19,10 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class PostService {
-    private final PostRepository postRepository;
     private final BoardRepository boardRepository;
+    private final PostRepository postRepository;
+    private final PostScrapRepository postScrapRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional(readOnly = true)
     public GetPostListResDto findPosts(Long boardId, Pageable pageable) {
@@ -29,4 +34,32 @@ public class PostService {
                 .map(GetPostResDto::from)
                 .collect(Collectors.toList()));
     }
+
+    @Transactional
+    public void postScrap(Long postId, Long memberId) {
+        PostScrap postScrap = postScrapRepository.findByPostIdAndMemberId(postId, memberId)
+                .orElse(null);
+        togglePostScrap(postId, memberId, postScrap);
+    }
+
+    private void togglePostScrap(Long postId, Long memberId, PostScrap postScrap) {
+        if (postScrap != null) {
+            deleteScrap(postScrap);
+            return;
+        }
+        saveScrap(postId, memberId);
+    }
+
+    private void saveScrap(Long postId, Long memberId) {
+        PostScrap postScrap = PostScrap.builder()
+                .post(postRepository.getReferenceById(postId))
+                .member(memberRepository.getReferenceById(memberId))
+                .build();
+        postScrapRepository.save(postScrap);
+    }
+
+    private void deleteScrap(PostScrap postScrap) {
+        postScrapRepository.delete(postScrap);
+    }
+
 }
