ac45374e3f16722e6fa3164ec1cc8bd9adb70531
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:18:39 +0900

    feat: 게시물 상세 보기 Controller 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafsound.domain.post.controller;
 
+import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
@@ -20,6 +21,19 @@ public class PostController {
     public EnvelopeResponse<GetPostListResDto> findPosts(@RequestParam Long boardId, Pageable pageable) {
         return EnvelopeResponse.<GetPostListResDto>builder()
                 .data(postService.findPosts(boardId, pageable))
+                .build();
+    }
+
+    @GetMapping("/{postId}")
+    public EnvelopeResponse findPost(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
+//        AuthenticatedMember authenticatedMember = AuthenticatedMember.builder()
+//                .memberId(1L)
+//                .memberRole("NORMAL")
+//                .build();
+//
+//        log.info("사용자 정보 : " + authenticatedMember.getMemberId() + " / " + authenticatedMember.getMemberRole());
+        return EnvelopeResponse.builder()
+                .data(postService.findPost(postId, authenticatedMember))
                 .build();
     }
 }
