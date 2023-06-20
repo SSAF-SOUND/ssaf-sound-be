2b86258c285f69accf78c8819a2dee8ab8dde4f0
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:15:41 +0900

    refactor: 매개변수 생성자 lombok 으로 변경

--- src/main/java/com/ssafy/ssafsound/domain/board/dto/GetBoardListResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/board/dto/GetBoardListResDto.java
@@ -1,15 +1,13 @@
 package com.ssafy.ssafsound.domain.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class GetBoardListResDto {
-
     private List<GetBoardResDto> boards;
 
-    public GetBoardListResDto(List<GetBoardResDto> boards) {
-        this.boards = boards;
-    }
 }
--- src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostListResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostListResDto.java
@@ -1,15 +1,12 @@
 package com.ssafy.ssafsound.domain.post.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class GetPostListResDto {
-
     private List<GetPostResDto> posts;
-
-    public GetPostListResDto(List<GetPostResDto> posts) {
-        this.posts = posts;
-    }
 }
