f2997eb0553422e851699ecdcce198a6e1d6c257
jjuny0310 <jjuny7712@gmail.com>
2023-06-21 02:11:25 +0900

    feat: 게시글 스크랩 Controller 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafsound.domain.post.controller;
 
+import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
@@ -20,6 +21,19 @@ public class PostController {
     public EnvelopeResponse<GetPostListResDto> findPosts(@RequestParam Long boardId, Pageable pageable) {
         return EnvelopeResponse.<GetPostListResDto>builder()
                 .data(postService.findPosts(boardId, pageable))
+                .build();
+    }
+
+    @PostMapping("{postId}/scrap")
+    public EnvelopeResponse<Void> postScrap(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
+        AuthenticatedMember tempMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .memberRole("NORMAL")
+                .build();
+        log.info("스크랩 실행");
+
+        postService.postScrap(postId, tempMember.getMemberId());
+        return EnvelopeResponse.<Void>builder()
                 .build();
     }
 }
