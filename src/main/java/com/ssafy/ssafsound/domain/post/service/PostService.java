9aba7f85cc90a5b972436d6d445bc5a8a2d63a92
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:18:16 +0900

    feat: 게시물 상세 보기 Service 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
@@ -1,23 +1,31 @@
 package com.ssafy.ssafsound.domain.post.service;
 
+import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.board.exception.BoardErrorInfo;
 import com.ssafy.ssafsound.domain.board.exception.BoardException;
 import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
+import com.ssafy.ssafsound.domain.post.domain.Post;
+import com.ssafy.ssafsound.domain.post.dto.GetPostDetailListResDto;
+import com.ssafy.ssafsound.domain.post.dto.GetPostDetailResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
+import com.ssafy.ssafsound.domain.post.exception.PostErrorInfo;
+import com.ssafy.ssafsound.domain.post.exception.PostException;
 import com.ssafy.ssafsound.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
-    private final PostRepository postRepository;
     private final BoardRepository boardRepository;
+    private final PostRepository postRepository;
+
 
     @Transactional(readOnly = true)
     public GetPostListResDto findPosts(Long boardId, Pageable pageable) {
@@ -27,6 +35,17 @@ public class PostService {
         return new GetPostListResDto(postRepository.findAllByBoardId(boardId, pageable)
                 .stream()
                 .map(GetPostResDto::from)
+                .collect(Collectors.toList()));
+    }
+
+    @Transactional(readOnly = true)
+    public GetPostDetailListResDto findPost(Long postId, AuthenticatedMember authenticatedMember) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(PostErrorInfo.INVALID_POST));
+
+        return new GetPostDetailListResDto(Optional.of(post)
+                .stream()
+                .map(p -> GetPostDetailResDto.from(p, authenticatedMember))
                 .collect(Collectors.toList()));
     }
 }
