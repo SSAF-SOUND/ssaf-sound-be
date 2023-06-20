b7b21aa872f695e344396b4ce3b1523e4c5c91c9
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:50:44 +0900

    refactor: 주석 삭제

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -4,7 +4,6 @@ import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.auth.validator.Authentication;
 import com.ssafy.ssafsound.domain.post.dto.GetPostDetailListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
-import com.ssafy.ssafsound.domain.post.dto.GetPostReqDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
@@ -32,12 +31,6 @@ public class PostController {
 
     @GetMapping("/{postId}")
     public EnvelopeResponse<GetPostDetailListResDto> findPost(@Authentication AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
-//        AuthenticatedMember authenticatedMember = AuthenticatedMember.builder()
-//                .memberId(1L)
-//                .memberRole("NORMAL")
-//                .build();
-//
-//        log.info("사용자 정보 : " + authenticatedMember.getMemberId() + " / " + authenticatedMember.getMemberRole());
         return EnvelopeResponse.<GetPostDetailListResDto>builder()
                 .data(postService.findPost(postId, authenticatedMember))
                 .build();
