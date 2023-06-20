b925eda03a5949882f3feee5e851286341b2c12b
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:49:28 +0900

    fix: 페이지네이션 외부에서 sort 못하도록 수정

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -1,12 +1,15 @@
 package com.ssafy.ssafsound.domain.post.controller;
 
 import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
+import com.ssafy.ssafsound.domain.auth.validator.Authentication;
 import com.ssafy.ssafsound.domain.post.dto.GetPostDetailListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
+import com.ssafy.ssafsound.domain.post.dto.GetPostReqDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +23,15 @@ public class PostController {
 
     @GetMapping
     public EnvelopeResponse<GetPostListResDto> findPosts(@RequestParam Long boardId, Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+
         return EnvelopeResponse.<GetPostListResDto>builder()
-                .data(postService.findPosts(boardId, pageable))
+                .data(postService.findPosts(boardId, pageRequest))
                 .build();
     }
 
     @GetMapping("/{postId}")
-    public EnvelopeResponse<GetPostDetailListResDto> findPost(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
+    public EnvelopeResponse<GetPostDetailListResDto> findPost(@Authentication AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
 //        AuthenticatedMember authenticatedMember = AuthenticatedMember.builder()
 //                .memberId(1L)
 //                .memberRole("NORMAL")
