8088b5445f94710e5306755c891493ed41d7199e
khs960616 <anjfkrhdkdle@naver.com>
2023-06-21 01:28:57 +0900

    fix: 모집 완료된 글에 대한 validation 추가 및 사용자 질문 validation 추가

--- src/main/java/com/ssafy/ssafsound/domain/recruitapplication/service/RecruitApplicationService.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruitapplication/service/RecruitApplicationService.java
@@ -1,9 +1,11 @@
 package com.ssafy.ssafsound.domain.recruitapplication.service;
 
 import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
+import com.ssafy.ssafsound.domain.meta.domain.MetaData;
 import com.ssafy.ssafsound.domain.meta.domain.MetaDataType;
 import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
 import com.ssafy.ssafsound.domain.recruit.domain.Recruit;
+import com.ssafy.ssafsound.domain.recruit.domain.RecruitLimitation;
 import com.ssafy.ssafsound.domain.recruit.domain.RecruitQuestion;
 import com.ssafy.ssafsound.domain.recruit.domain.RecruitQuestionReply;
 import com.ssafy.ssafsound.domain.recruit.exception.RecruitErrorInfo;
@@ -20,6 +22,7 @@ import com.ssafy.ssafsound.global.common.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,6 +60,7 @@ public class RecruitApplicationService {
         RecruitApplication recruitApplication = recruitApplicationRepository.findByIdFetchRecruitWriter(recruitApplicationId)
                 .orElseThrow(() -> new ResourceNotFoundException(GlobalErrorInfo.NOT_FOUND));
 
+        getNotFullRecruitLimitation(recruitApplication.getRecruit(), recruitApplication.getType());
         changeRecruitApplicationState(recruitApplication, memberId, status,
                 (entity, mid) -> {
                     boolean isNotRegister = !entity.getRecruit().getMember().getId().equals(mid);
@@ -70,6 +74,8 @@ public class RecruitApplicationService {
         RecruitApplication recruitApplication = recruitApplicationRepository.findByIdAndMemberId(recruitApplicationId, memberId)
                 .orElseThrow(()->new ResourceNotFoundException(GlobalErrorInfo.NOT_FOUND));
 
+        RecruitLimitation recruitLimitation = getNotFullRecruitLimitation(recruitApplication.getRecruit(), recruitApplication.getType());
+        recruitLimitation.increaseCurrentNumber();
         changeRecruitApplicationState(recruitApplication, memberId, status,
                 (entity, mid)-> !entity.getMatchStatus().equals(MatchStatus.WAITING_APPLICANT));
     }
@@ -106,8 +112,26 @@ public class RecruitApplicationService {
     }
 
     private boolean isRecruitingType(String recruitType, Recruit recruit) {
-        return recruit.getLimitations().stream()
-                .anyMatch(limit -> limit.getType().getName().equals(recruitType));
+        List<RecruitLimitation> recruitLimitations = recruit.getLimitations();
+
+        for(RecruitLimitation recruitLimitation: recruitLimitations) {
+            if(!recruitLimitation.getType().getName().equals(recruitType)) continue;
+            if(recruitLimitation.getLimitation() < recruitLimitation.getCurrentNumber()) return true;
+            throw new RecruitException(RecruitErrorInfo.IS_ALREADY_FULL);
+        }
+        return false;
+    }
+
+    private RecruitLimitation getNotFullRecruitLimitation(Recruit recruit, MetaData recruitType) {
+        RecruitLimitation recruitLimitation = recruit.getLimitations()
+                .stream()
+                .filter(limit -> limit.getType() == recruitType)
+                .findFirst().orElseThrow(()->new ResourceNotFoundException(GlobalErrorInfo.NOT_FOUND));
+
+        if(recruitLimitation.getCurrentNumber() >= recruitLimitation.getLimitation()) {
+            throw new RecruitException(RecruitErrorInfo.IS_ALREADY_FULL);
+        }
+        return recruitLimitation;
     }
 
     private List<RecruitQuestionReply>  makeRecruitQuestionReplies(PostRecruitApplicationReqDto dto, Recruit recruit, RecruitApplication recruitApplication) {
@@ -121,6 +145,10 @@ public class RecruitApplicationService {
         }
 
         for(int i=0; i<len; ++i) {
+            if(!StringUtils.hasText(dto.getContents().get(i))) {
+                throw new RecruitException(RecruitErrorInfo.NOT_SAME_LENGTH_RECRUIT_QUESTION_ANSWER);
+            }
+
             replies.add(
                     RecruitQuestionReply.builder()
                             .application(recruitApplication)
