8d5b53be2ca7597a977187272c1897657d48ec1b
jjuny0310 <jjuny7712@gmail.com>
2023-06-20 23:22:32 +0900

    fix: Controller 리턴타입 수정

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.post.controller;
 
 import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
+import com.ssafy.ssafsound.domain.post.dto.GetPostDetailListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
@@ -25,14 +26,14 @@ public class PostController {
     }
 
     @GetMapping("/{postId}")
-    public EnvelopeResponse findPost(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
+    public EnvelopeResponse<GetPostDetailListResDto> findPost(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
 //        AuthenticatedMember authenticatedMember = AuthenticatedMember.builder()
 //                .memberId(1L)
 //                .memberRole("NORMAL")
 //                .build();
 //
 //        log.info("사용자 정보 : " + authenticatedMember.getMemberId() + " / " + authenticatedMember.getMemberRole());
-        return EnvelopeResponse.builder()
+        return EnvelopeResponse.<GetPostDetailListResDto>builder()
                 .data(postService.findPost(postId, authenticatedMember))
                 .build();
     }
