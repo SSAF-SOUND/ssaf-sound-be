663f6f9d5a60f0ebe59988d319eb01f20892f0aa
jjuny0310 <jjuny7712@gmail.com>
2023-06-21 21:02:32 +0900

    feat: 게시글 좋아요 Repository 작성

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/repository/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.ssafsound.domain.post.repository;
+
+import com.ssafy.ssafsound.domain.post.domain.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    Optional<PostLike> findByPostIdAndMemberId(Long postId, Long memberId);
+}
