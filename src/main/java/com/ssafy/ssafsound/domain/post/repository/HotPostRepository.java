6960cc11e51575d632590c54f03a48e321e53c99
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-06-23 00:25:38 +0900

    feat: 게시글 좋아요 기능 구현
    * feat: 게시글 좋아요 Repository 작성
    
    * feat: 게시글 좋아요 Service 구현
    
    * feat: 게시글 좋아요 Controller 구현
    
    * feat: Hot 게시글 Repository 작성
    
    * feat: countByPostId 추가
    
    * feat: 정해진 좋아요 개수를 달성하면 Hot 게시글에 등록되는 로직 구현
    
    * refactor: Hot 게시글 좋아요 기준 상수로 변경
    
    * chore: Spring boot batch 관련 의존성 추가
    
    * chore: batch 관련 설정 및 상수 추가
    
    * feat: @EnableBatchProcessing 추가
    
    * feat: 좋아요가 threshold 보다 낮은 Hot 게시글 삭제 기능 구현
    
    * feat: Hot 게시글 삭제 Batch 자동화 구현(오전 4시마다 실행)
    
    * refactor: Hot 게시글 삭제 메소드 네이밍 수정
    
    * fix: 테스트용 코드 제거
    
    * refactor: Hot 게시글 삭제 관련 메소드 네이밍 변경
    
    * chore: 설정파일 제외
    
    * chore: import 파일 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/repository/HotPostRepository.java
@@ -0,0 +1,22 @@
+package com.ssafy.ssafsound.domain.post.repository;
+
+import com.ssafy.ssafsound.domain.post.domain.HotPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotPostRepository extends JpaRepository<HotPost, Long> {
+    @Modifying
+    @Query("DELETE FROM hot_post h " +
+            "WHERE h.id IN ( " +
+            "  SELECT h.id " +
+            "  FROM hot_post h LEFT JOIN post_like p " +
+            "  ON h.post.id = p.post.id " +
+            "  GROUP BY h.id " +
+            "  HAVING COUNT(p.id) < :threshold " +
+            ")")
+    void deleteBelowThresholdHotPosts(@Param("threshold") Long threshold);
+}
