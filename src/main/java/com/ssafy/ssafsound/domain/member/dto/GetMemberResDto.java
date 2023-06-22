c880284709a9106c1aedc059cb14587e0731af63
김태용 <amico741@naver.com>
2023-06-22 21:04:05 +0900

    fix: 멤버 정보 조회 response dto 구조 변경

---
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/GetMemberResDto.java
@@ -0,0 +1,40 @@
+package com.ssafy.ssafsound.domain.member.dto;
+
+import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.member.domain.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class GetMemberResDto {
+
+    private Long memberId;
+    private String memberRole;
+    private String nickname;
+    private Boolean ssafyMember;
+    private SSAFYInfo ssafyInfo;
+
+    public static GetMemberResDto fromGeneralUser(Member member, MemberRole memberRole) {
+        return GetMemberResDto.builder()
+                .memberId(member.getId())
+                .memberRole(memberRole.getRoleType())
+                .nickname(member.getNickname())
+                .ssafyMember(member.getSsafyMember())
+                .build();
+    }
+
+    public static GetMemberResDto fromSSAFYUser(Member member, MemberRole memberRole) {
+        return GetMemberResDto.builder()
+                .memberId(member.getId())
+                .memberRole(memberRole.getRoleType())
+                .nickname(member.getNickname())
+                .ssafyMember(member.getSsafyMember())
+                .ssafyInfo(new SSAFYInfo(member.getSemester(), member.getMajor(), member.getCampus().getName(), member.getCertificationState().name()))
+                .build();
+    }
+}
