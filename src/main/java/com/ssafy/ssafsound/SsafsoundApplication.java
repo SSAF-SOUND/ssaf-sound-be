7401e11241e5593a0ad946cc5e86622da5cf0f62
jjuny0310 <jjuny7712@gmail.com>
2023-06-22 19:39:58 +0900

    feat: @EnableBatchProcessing 추가

--- src/main/java/com/ssafy/ssafsound/SsafsoundApplication.java
+++ src/main/java/com/ssafy/ssafsound/SsafsoundApplication.java
@@ -1,10 +1,12 @@
 package com.ssafy.ssafsound;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableBatchProcessing
 @EnableJpaAuditing
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SsafsoundApplication {
