8552c7abb1f4ab76495be5a97a5c34f48f75b88f
jjuny0310 <jjuny7712@gmail.com>
2023-06-24 03:43:57 +0900

    feat: 게시글 쓰기 Controller 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.ssafy.ssafsound.domain.post.controller;
 import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.post.dto.GetPostDetailListResDto;
 import com.ssafy.ssafsound.domain.post.dto.GetPostListResDto;
+import com.ssafy.ssafsound.domain.post.dto.PostPostWriteReqDto;
 import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -30,22 +34,59 @@ public class PostController {
 
     @GetMapping("/{postId}")
     public EnvelopeResponse<GetPostDetailListResDto> findPost(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
+        AuthenticatedMember testMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .memberRole("NORMAL")
+                .build();
+
         return EnvelopeResponse.<GetPostDetailListResDto>builder()
-                .data(postService.findPost(postId, authenticatedMember))
+                .data(postService.findPost(postId, testMember))
                 .build();
     }
 
     @PostMapping("/{postId}/like")
     public EnvelopeResponse<Void> postLike(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
-        postService.postLike(postId, authenticatedMember.getMemberId());
+        AuthenticatedMember testMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .memberRole("NORMAL")
+                .build();
+
+        postService.postLike(postId, testMember.getMemberId());
         return EnvelopeResponse.<Void>builder()
                 .build();
     }
 
     @PostMapping("/{postId}/scrap")
     public EnvelopeResponse<Void> postScrap(AuthenticatedMember authenticatedMember, @PathVariable Long postId) {
-        postService.postScrap(postId, authenticatedMember.getMemberId());
+        AuthenticatedMember testMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .memberRole("NORMAL")
+                .build();
+
+        postService.postScrap(postId, testMember.getMemberId());
         return EnvelopeResponse.<Void>builder()
+                .build();
+    }
+
+    @PostMapping
+    public EnvelopeResponse writePost(@RequestPart("data") PostPostWriteReqDto postPostWriteReqDto, @RequestPart List<MultipartFile> images,
+                                      @RequestParam Long boardId, AuthenticatedMember authenticatedMember) {
+        AuthenticatedMember testMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .memberRole("NORMAL")
+                .build();
+
+        log.info("글쓰기 컨트롤러");
+        log.info("boardId: " + boardId);
+        log.info("제목 : " + postPostWriteReqDto.getTitle());
+        log.info("내용 : " + postPostWriteReqDto.getContent());
+        log.info("익명 : " + postPostWriteReqDto.isAnonymous());
+        for (MultipartFile image : images) {
+            log.info("이미지 : " + image.getOriginalFilename());
+        }
+
+        return EnvelopeResponse.builder()
+                .data(postService.writePost(boardId, postPostWriteReqDto, images, testMember.getMemberId()))
                 .build();
     }
 }
