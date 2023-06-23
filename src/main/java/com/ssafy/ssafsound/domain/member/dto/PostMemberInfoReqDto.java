2997fc42c17253eacef16cba067d7db7e31851e1
김태용 <amico741@naver.com>
2023-06-23 10:28:19 +0900

    feat: 멤버 기본 정보 생성 요청 dto 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/PostMemberInfoReqDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.ssafsound.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Builder
+@Getter
+public class PostMemberInfoReqDto {
+
+    @NotNull
+    @Size(min = 1, max = 11)
+    private String nickname;
+
+    @NotNull
+    private Boolean ssafyMember;
+
+    private Integer semester;
+
+    private Boolean isMajor;
+
+    private String campus;
+}
