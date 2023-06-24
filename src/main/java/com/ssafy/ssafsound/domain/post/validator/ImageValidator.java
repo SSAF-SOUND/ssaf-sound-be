3b1800b24d0b38439a7f61afa93cfc967b5e436a
jjuny0310 <jjuny7712@gmail.com>
2023-06-25 04:56:29 +0900

    feat: 파일 형식을 통한 이미지 검증 Validator 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/validator/ImageValidator.java
@@ -0,0 +1,34 @@
+package com.ssafy.ssafsound.domain.post.validator;
+
+import org.apache.tika.Tika;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.List;
+
+public class ImageValidator implements ConstraintValidator<CheckImage, List<MultipartFile>> {
+
+    @Override
+    public boolean isValid(List<MultipartFile> files, ConstraintValidatorContext context) {
+        if (files.get(0).isEmpty()) {
+            return true;
+        }
+
+        for (MultipartFile file : files) {
+            try {
+                Tika tika = new Tika();
+                String mimeType = tika.detect(file.getBytes());
+                if (!mimeType.startsWith("image/"))
+                    return false;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return true;
+    }
+}
