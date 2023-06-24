2f6188fc5cac886e5ec181f64e855927a234a3cc
jjuny0310 <jjuny7712@gmail.com>
2023-06-25 04:07:45 +0900

    feat: 파일 크기 Validator 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/validator/CheckFileSize.java
@@ -0,0 +1,19 @@
+package com.ssafy.ssafsound.domain.post.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FileSizeValidator.class)
+public @interface CheckFileSize {
+    String message() default "파일이 제한된 용량을 초과했습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    long maxFileSize(); // 최대 파일 용량
+}
