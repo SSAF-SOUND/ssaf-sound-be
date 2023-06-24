378706a6f8ff68a1761768e92b3578be9af9fc3f
jjuny0310 <jjuny7712@gmail.com>
2023-06-25 01:16:28 +0900

    fix: 이미지 파일 분리

--- src/main/java/com/ssafy/ssafsound/domain/post/dto/PostPostWriteReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/PostPostWriteReqDto.java
@@ -2,11 +2,25 @@ package com.ssafy.ssafsound.domain.post.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 public class PostPostWriteReqDto {
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String title;
+
+    @NotBlank
+    @Size(min = 2)
     private String content;
+
     private boolean anonymous;
 }
