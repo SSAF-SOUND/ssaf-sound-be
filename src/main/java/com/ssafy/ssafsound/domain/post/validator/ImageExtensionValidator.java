12b935660d143f5be72c43ca31ffed383f0ae9c2
jjuny0310 <jjuny7712@gmail.com>
2023-06-25 04:55:38 +0900

    feat: 이미지 확장자 검증 Validator 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/validator/ImageExtensionValidator.java
@@ -0,0 +1,30 @@
+package com.ssafy.ssafsound.domain.post.validator;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class ImageExtensionValidator implements ConstraintValidator<CheckImageExtension, List<MultipartFile>> {
+
+    @Override
+    public boolean isValid(List<MultipartFile> files, ConstraintValidatorContext context) {
+        if (files.get(0).isEmpty()) {
+            return true;
+        }
+
+        List<String> imageExtensions = Arrays.asList("jpg", "jpeg", "png", "bmp", "webp");
+
+        for (MultipartFile file : files) {
+            String fileName = file.getOriginalFilename();
+            String extension = Objects.requireNonNull(fileName).substring(fileName.lastIndexOf(".") + 1);
+            if (!imageExtensions.contains(extension.toLowerCase()))
+                return false;
+        }
+
+        return true;
+    }
+}
