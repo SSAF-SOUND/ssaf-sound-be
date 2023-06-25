0bc8e0d6d4f4b9ad917d376baf35bcac34028c60
김태용 <amico741@naver.com>
2023-06-25 17:01:09 +0900

    feat: 닉네임 중복 검사 컨트롤러 추가

--- src/main/java/com/ssafy/ssafsound/domain/member/controller/MemberController.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
 import com.ssafy.ssafsound.domain.auth.validator.Authentication;
 import com.ssafy.ssafsound.domain.member.dto.GetMemberResDto;
 import com.ssafy.ssafsound.domain.member.dto.PostMemberInfoReqDto;
+import com.ssafy.ssafsound.domain.member.dto.PostNicknameReqDto;
+import com.ssafy.ssafsound.domain.member.dto.PostNicknameResDto;
 import com.ssafy.ssafsound.domain.member.service.MemberService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +32,13 @@ public class MemberController {
                                                                        @Valid @RequestBody PostMemberInfoReqDto postMemberInfoReqDto) {
         return EnvelopeResponse.<GetMemberResDto>builder()
                 .data(memberService.registerMemberInformation(authenticatedMember, postMemberInfoReqDto))
+                .build();
+    }
+
+    @PostMapping("/nickname")
+    public EnvelopeResponse<PostNicknameResDto> checkNicknamePossible(@Valid @RequestBody PostNicknameReqDto postNicknameReqDto) {
+        return EnvelopeResponse.<PostNicknameResDto>builder()
+                .data(memberService.checkNicknamePossible(postNicknameReqDto))
                 .build();
     }
 }
