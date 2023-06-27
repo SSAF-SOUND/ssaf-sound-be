da29421228051b70fd58af2d0e69cc7349adedee
jjuny0310 <jjuny7712@gmail.com>
2023-06-27 23:58:11 +0900

    feat: 게시글의 작성자를 함께 가져오는 쿼리문 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/repository/PostRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/repository/PostRepository.java
@@ -3,11 +3,17 @@ package com.ssafy.ssafsound.domain.post.repository;
 import com.ssafy.ssafsound.domain.post.domain.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByBoardId(Long boardId, Pageable pageable);
+
+    @Query("SELECT p FROM post p JOIN FETCH p.member WHERE p.id = :id")
+    Optional<Post> findByIdWithMember(@Param("id") Long id);
 }
