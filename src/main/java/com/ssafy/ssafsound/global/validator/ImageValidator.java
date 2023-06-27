5efe7b4ecffd44192989c6cca3bacc5229b5de19
jjuny0310 <jjuny7712@gmail.com>
2023-06-27 20:50:05 +0900

    refactor: 확장자 검증, 타입 검증 결합

--- src/main/java/com/ssafy/ssafsound/global/validator/ImageValidator.java
+++ src/main/java/com/ssafy/ssafsound/global/validator/ImageValidator.java
@@ -6,7 +6,9 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ImageValidator implements ConstraintValidator<CheckImage, List<MultipartFile>> {
 
@@ -16,8 +18,18 @@ public class ImageValidator implements ConstraintValidator<CheckImage, List<Mult
             return true;
         }
 
+        List<String> imageExtensions = Arrays.asList("jpg", "jpeg", "png", "bmp", "webp");
+
+
         for (MultipartFile file : files) {
+            // 확장자 검증
+            String fileName = file.getOriginalFilename();
+            String extension = Objects.requireNonNull(fileName).substring(fileName.lastIndexOf(".") + 1);
+            if (!imageExtensions.contains(extension.toLowerCase()))
+                return false;
+
             try {
+                // 파일 타입 검증
                 Tika tika = new Tika();
                 String mimeType = tika.detect(file.getBytes());
                 if (!mimeType.startsWith("image/"))
