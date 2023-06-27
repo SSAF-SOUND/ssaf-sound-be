8dda5a129b8627dcb647d22a1bac27ddce15f5d2
jjuny0310 <jjuny7712@gmail.com>
2023-06-27 23:59:50 +0900

    feat: 삭제 시 작성자가 아니면 예외 발생

--- src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum PostErrorInfo {
-    NOT_FOUND("802", "게시물을 찾을 수 없습니다."),
-    DUPLICATE_REPORT("803", "해당 게시글은 이미 신고 되었습니다.");
+    NOT_FOUND("802", "게시글을 찾을 수 없습니다."),
+    DUPLICATE_REPORT("803", "이미 신고된 게시글입니다."),
+    UNAUTHORIZED_DELETE_POST("804", "해당 게시글 삭제 권한이 없습니다.");
 
 
     private final String code;
