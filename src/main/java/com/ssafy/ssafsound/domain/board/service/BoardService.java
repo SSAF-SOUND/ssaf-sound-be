ae5c1f1da142daa6ceb7f0c1a0de23146a223477
jjuny0310 <jjuny7712@gmail.com>
2023-06-29 03:06:32 +0900

    feat: 게시판이 비어있을 때 예외 처리

--- src/main/java/com/ssafy/ssafsound/domain/board/service/BoardService.java
+++ src/main/java/com/ssafy/ssafsound/domain/board/service/BoardService.java
@@ -1,26 +1,37 @@
 package com.ssafy.ssafsound.domain.board.service;
 
-import com.ssafy.ssafsound.domain.board.dto.GetBoardListResDto;
 import com.ssafy.ssafsound.domain.board.dto.GetBoardResDto;
+import com.ssafy.ssafsound.domain.board.dto.GetBoard;
+import com.ssafy.ssafsound.domain.board.exception.BoardErrorInfo;
+import com.ssafy.ssafsound.domain.board.exception.BoardException;
 import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
 
     @Transactional(readOnly = true)
-    public GetBoardListResDto findBoards() {
-        return GetBoardListResDto.builder()
-                .boards(boardRepository.findAll()
-                        .stream()
-                        .map(GetBoardResDto::fromEntity)
-                        .collect(Collectors.toList()))
+    public GetBoardResDto findBoards() {
+        List<GetBoard> boards = boardRepository.findAll()
+                .stream()
+                .map(board -> GetBoard.from(board))
+                .collect(Collectors.toList());
+
+        if (boards.size() == 0) {
+            throw new BoardException(BoardErrorInfo.EMPTY_BOARD);
+        }
+
+        return GetBoardResDto.builder()
+                .boards(boards)
                 .build();
     }
 }
