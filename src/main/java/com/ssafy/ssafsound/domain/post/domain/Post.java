8e8f08885d8bc1270e228e113fa404d6fe6298f5
jjuny0310 <jjuny7712@gmail.com>
2023-06-28 22:06:10 +0900

    feat: setter 추가

--- src/main/java/com/ssafy/ssafsound/domain/post/domain/Post.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/domain/Post.java
@@ -4,10 +4,7 @@ import com.ssafy.ssafsound.domain.BaseTimeEntity;
 import com.ssafy.ssafsound.domain.board.domain.Board;
 import com.ssafy.ssafsound.domain.comment.domain.Comment;
 import com.ssafy.ssafsound.domain.member.domain.Member;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -17,6 +14,7 @@ import java.util.List;
 
 @Entity(name="post")
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -38,6 +36,7 @@ public class Post extends BaseTimeEntity {
     @Column
     private Long view;
 
+    @Builder.Default
     @Column
     private Boolean deletedPost = Boolean.FALSE;
 
@@ -55,15 +54,19 @@ public class Post extends BaseTimeEntity {
     @OneToOne(mappedBy = "post", cascade = CascadeType.REMOVE)
     private HotPost hotPost;
 
+    @Builder.Default
     @OneToMany(mappedBy = "post")
     private List<PostImage> images = new ArrayList<>();
 
+    @Builder.Default
     @OneToMany(mappedBy = "post")
     private List<PostLike> likes = new ArrayList<>();
 
+    @Builder.Default
     @OneToMany(mappedBy = "post")
     private List<Comment> comments = new ArrayList<>();
 
+    @Builder.Default
     @OneToMany(mappedBy = "post")
     private List<PostScrap> scraps = new ArrayList<>();
 
