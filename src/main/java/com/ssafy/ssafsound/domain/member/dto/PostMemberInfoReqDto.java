c69c251f76be84ace0b8b49f78a6ad71f15a4713
김태용 <amico741@naver.com>
2023-06-29 12:37:22 +0900

    feat: 생성자 관련 롬복 추가

--- src/main/java/com/ssafy/ssafsound/domain/member/dto/PostMemberInfoReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/PostMemberInfoReqDto.java
@@ -1,12 +1,17 @@
 package com.ssafy.ssafsound.domain.member.dto;
 
 import com.ssafy.ssafsound.domain.member.validator.Semester;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class PostMemberInfoReqDto {
 
