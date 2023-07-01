6e85466fc89e0407ab8e0ef1f24fdebc07f4b2fd
jjuny0310 <jjuny7712@gmail.com>
2023-07-02 00:44:30 +0900

    feat: 댓글 쓰기 Service 구현

---
+++ src/main/java/com/ssafy/ssafsound/domain/comment/service/CommentService.java
@@ -0,0 +1,61 @@
+package com.ssafy.ssafsound.domain.comment.service;
+
+import com.ssafy.ssafsound.domain.comment.domain.Comment;
+import com.ssafy.ssafsound.domain.comment.domain.CommentNumber;
+import com.ssafy.ssafsound.domain.comment.dto.PostCommentWriteReqDto;
+import com.ssafy.ssafsound.domain.comment.exception.CommentErrorInfo;
+import com.ssafy.ssafsound.domain.comment.exception.CommentException;
+import com.ssafy.ssafsound.domain.comment.repository.CommentNumberRepository;
+import com.ssafy.ssafsound.domain.comment.repository.CommentRepository;
+import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNumberRepository commentNumberRepository;
+    private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
+
+
+    @Transactional
+    public Long writeComment(Long postId, Long memberId, PostCommentWriteReqDto postCommentWriteReqDto) {
+        // 1. 익명 번호 부여
+        CommentNumber commentNumber = null;
+        if (!commentNumberRepository.existsByPostIdAndMemberId(postId, memberId)) {
+            commentNumber = CommentNumber.builder()
+                    .post(postRepository.getReferenceById(postId))
+                    .member(memberRepository.getReferenceById(memberId))
+                    .number(commentNumberRepository.countAllByPostId(postId) + 1)
+                    .build();
+            commentNumberRepository.save(commentNumber);
+        }
+
+
+        // 2. 댓글 저장
+        if (commentNumber == null) {
+            commentNumber = commentNumberRepository
+                    .findByPostIdAndMemberId(postId, memberId)
+                    .orElseThrow(() -> new CommentException(CommentErrorInfo.NOT_FOUND_COMMENT_NUMBER));
+        }
+
+        Comment comment = Comment.builder()
+                .post(postRepository.getReferenceById(postId))
+                .member(memberRepository.getReferenceById(memberId))
+                .content(postCommentWriteReqDto.getContent())
+                .parent(true)
+                .anonymous(postCommentWriteReqDto.getAnonymous())
+                .commentNumber(commentNumber)
+                .commentGroup(null)
+                .build();
+
+        return commentRepository.save(comment).getId();
+    }
+
+}
