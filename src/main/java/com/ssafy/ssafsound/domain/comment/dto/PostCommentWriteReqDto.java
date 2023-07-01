3f3591ca9cff0543bfeab2132b90e744f6fe07d7
jjuny0310 <jjuny7712@gmail.com>
2023-07-01 22:11:24 +0900

    feat: 댓글 작성 req dto 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/comment/dto/PostCommentWriteReqDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafsound.domain.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class PostCommentWriteReqDto {
+    @Size(min = 2)
+    private String content;
+}
