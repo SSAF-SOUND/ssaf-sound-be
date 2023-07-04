825abe2481b32d3f2c8f7b6fd843363db016a7d9
moonn6pence <shin421179@gmail.com>
2023-07-04 19:59:38 +0900

    feat : 쪽지 관련 엔티티 수정 및 추가
    - chat_report 엔티티 추가
    - chat 엔티티 어노테이션 추가

--- src/main/java/com/ssafy/ssafsound/domain/chat/domain/ChatReport.java
+++ src/main/java/com/ssafy/ssafsound/domain/chat/domain/ChatReport.java
@@ -1,33 +1,37 @@
 package com.ssafy.ssafsound.domain.chat.domain;
 
-import com.ssafy.ssafsound.domain.BaseTimeEntity;
+import com.ssafy.ssafsound.domain.member.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-@Entity(name = "chat")
+@Entity(name = "chat_report")
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Chat extends BaseTimeEntity {
+public class ChatReport {
 
     @Id
-    @Column(name = "chat_id")
+    @Column(name = "chat_report_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "chat_room_id")
+    @JoinColumn
     private ChatRoom chatRoom;
 
     @ManyToOne
-    @JoinColumn(name = "talker_id")
-    private Talker talker;
+    @JoinColumn
+    private Member member;
 
     @Column
-    private String message;
+    private String reason;
+
+    @Column
+    private LocalDateTime created_at;
 }
