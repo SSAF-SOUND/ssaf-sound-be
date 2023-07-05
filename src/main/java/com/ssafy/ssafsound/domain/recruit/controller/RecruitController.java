2d78c34059982d39a8770d9a3c06fcd2631409c3
khs960616 <55014803+khs960616@users.noreply.github.com>
2023-07-05 20:43:39 +0900

    fix: SSAFY Info 수정에 따른 Response Body 데이터 변경
    * chore: 미사용 import 제거
    
    * refactor: 데이터 구조 변경
    
    * refactor: response 데이터 구조 변경

--- src/main/java/com/ssafy/ssafsound/domain/recruit/controller/RecruitController.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruit/controller/RecruitController.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.recruit.controller;
 
 import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
+import com.ssafy.ssafsound.domain.auth.validator.Authentication;
 import com.ssafy.ssafsound.domain.recruit.dto.*;
 import com.ssafy.ssafsound.domain.recruit.service.RecruitService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
@@ -18,13 +19,13 @@ public class RecruitController {
     private final RecruitService recruitService;
 
     @PostMapping
-    public EnvelopeResponse<Void> saveRecruit(AuthenticatedMember memberInfo, @Valid @RequestBody PostRecruitReqDto recruitReqDto) {
+    public EnvelopeResponse<Void> saveRecruit(@Authentication AuthenticatedMember memberInfo, @Valid @RequestBody PostRecruitReqDto recruitReqDto) {
         recruitService.saveRecruit(memberInfo.getMemberId(), recruitReqDto);
         return EnvelopeResponse.<Void>builder().build();
     }
 
     @PostMapping("/{recruitId}/scrap")
-    public EnvelopeResponse<Void> toggleRecruitScrap(@PathVariable Long recruitId, AuthenticatedMember memberInfo) {
+    public EnvelopeResponse<Void> toggleRecruitScrap(@PathVariable Long recruitId, @Authentication AuthenticatedMember memberInfo) {
         recruitService.toggleRecruitScrap(recruitId, memberInfo.getMemberId());
         return EnvelopeResponse.<Void>builder().build();
     }
@@ -37,13 +38,13 @@ public class RecruitController {
     }
 
     @PatchMapping("/{recruitId}")
-    public EnvelopeResponse<Void> updateRecruit(@PathVariable Long recruitId, AuthenticatedMember memberInfo, @RequestBody PatchRecruitReqDto patchRecruitReqDto) {
+    public EnvelopeResponse<Void> updateRecruit(@PathVariable Long recruitId, @Authentication AuthenticatedMember memberInfo, @RequestBody PatchRecruitReqDto patchRecruitReqDto) {
         recruitService.updateRecruit(recruitId, memberInfo.getMemberId(), patchRecruitReqDto);
         return EnvelopeResponse.<Void>builder().build();
     }
 
     @DeleteMapping("/{recruitId}")
-    public EnvelopeResponse<Void> deleteRecruit(@PathVariable Long recruitId, AuthenticatedMember memberInfo) {
+    public EnvelopeResponse<Void> deleteRecruit(@PathVariable Long recruitId, @Authentication AuthenticatedMember memberInfo) {
         recruitService.deleteRecruit(recruitId, memberInfo.getMemberId());
         return EnvelopeResponse.<Void>builder().build();
     }
--- src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/RecruitParticipantElement.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/RecruitParticipantElement.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.recruitapplication.dto;
 
 import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.member.dto.SSAFYInfo;
 import com.ssafy.ssafsound.domain.recruit.domain.Recruit;
 import com.ssafy.ssafsound.domain.recruitapplication.domain.RecruitApplication;
 import lombok.Builder;
@@ -11,48 +12,49 @@ import lombok.Getter;
 public class RecruitParticipantElement {
     private String recruitType;
     private Long memberId;
-    private Integer year;
     private String nickName;
-    private String certificationState;
-    private Boolean ssafyMember;
-    private Boolean major;
-    private String majorType;
+    private Boolean isMajor;
+    private SSAFYInfo ssafyInfo;
 
     public static RecruitParticipantElement from(RecruitApplication recruitApplication) {
         Member member = recruitApplication.getMember();
-        String majorType = null;
-        if(member.getMajorType() != null) {
-            majorType = member.getMajorType().getName();
+        SSAFYInfo ssafyInfo = null;
+        if(member.getSsafyMember()) {
+            ssafyInfo = SSAFYInfo.builder()
+                    .semester(member.getSemester())
+                    .campus(member.getCampus().getName())
+                    .certificationState(member.getCertificationState().name())
+                    .majorType(member.getMajorType().getName())
+                    .build();
         }
 
         return RecruitParticipantElement.builder()
                 .recruitType(recruitApplication.getType().getName())
                 .memberId(member.getId())
-                .year(member.getSemester())
                 .nickName(member.getNickname())
-                .certificationState(member.getCertificationState().name())
-                .ssafyMember(member.getSsafyMember())
-                .major(member.getMajor())
-                .majorType(majorType)
+                .isMajor(member.getMajor())
+                .ssafyInfo(ssafyInfo)
                 .build();
     }
 
     public static RecruitParticipantElement from(Recruit recruit) {
         Member register = recruit.getMember();
-        String majorType = null;
-        if(register.getMajorType() != null) {
-            majorType = register.getMajorType().getName();
+        SSAFYInfo ssafyInfo = null;
+        if(register.getSsafyMember()) {
+            ssafyInfo = SSAFYInfo.builder()
+                    .semester(register.getSemester())
+                    .campus(register.getCampus().getName())
+                    .certificationState(register.getCertificationState().name())
+                    .majorType(register.getMajorType().getName())
+                    .build();
         }
 
         return RecruitParticipantElement.builder()
                 .recruitType(recruit.getRegisterRecruitType().getName())
                 .memberId(register.getId())
-                .year(register.getSemester())
                 .nickName(register.getNickname())
-                .certificationState(register.getCertificationState().name())
-                .ssafyMember(register.getSsafyMember())
-                .major(register.getMajor())
-                .majorType(majorType)
+                .isMajor(register.getMajor())
+                .ssafyInfo(ssafyInfo)
                 .build();
     }
 }
