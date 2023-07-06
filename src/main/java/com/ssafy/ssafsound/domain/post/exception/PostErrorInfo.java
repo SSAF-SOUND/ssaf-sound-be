60063f342a9beef7f0b95dc9663634c7e16308b9
jjuny7712 <jjuny7712@gmail.com>
2023-07-06 17:23:40 +0900

    fix: 게시글 조회 결과 없을 때 예외 메시지 삭제

--- src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
@@ -5,13 +5,10 @@ import lombok.Getter;
 @Getter
 public enum PostErrorInfo {
     NOT_FOUND_POST("802", "게시글을 찾을 수 없습니다."),
-    NOT_FOUND_POSTS("803", "게시글 목록을 찾을 수 없습니다."),
-    DUPLICATE_REPORT("804", "이미 신고된 게시글입니다."),
-    UNABLE_REPORT_MY_POST("805", "자신의 게시글은 신고할 수 없습니다."),
-    UNAUTHORIZED_DELETE_POST("806", "해당 게시글 삭제 권한이 없습니다."),
-    UNAUTHORIZED_UPDATE_POST("807", "해당 게시글 수정 권한이 없습니다."),
-    NOT_FOUND_POSTS_SEARCH_RESULT("809", "검색 결과를 찾을 수 없습니다");
-
+    DUPLICATE_REPORT("803", "이미 신고된 게시글입니다."),
+    UNABLE_REPORT_MY_POST("804", "자신의 게시글은 신고할 수 없습니다."),
+    UNAUTHORIZED_DELETE_POST("805", "해당 게시글 삭제 권한이 없습니다."),
+    UNAUTHORIZED_UPDATE_POST("806", "해당 게시글 수정 권한이 없습니다.");
 
     private final String code;
     private final String message;
