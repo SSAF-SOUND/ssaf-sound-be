eafe2505023d05b9d9845b6562376e4db7f1b8d4
김태용 <amico741@naver.com>
2023-07-07 01:00:02 +0900

    feat: 생성자 Lombok 추가

--- src/main/java/com/ssafy/ssafsound/domain/member/dto/PostCertificationInfoReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/PostCertificationInfoReqDto.java
@@ -1,12 +1,16 @@
 package com.ssafy.ssafsound.domain.member.dto;
 
 import com.ssafy.ssafsound.domain.member.validator.Semester;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class PostCertificationInfoReqDto {
 
