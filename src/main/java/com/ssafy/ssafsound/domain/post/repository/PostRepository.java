7ea5ea5d1ff689739c63de09c8097895c703db92
jjuny7712 <jjuny7712@gmail.com>
2023-07-06 16:50:07 +0900

    feat: 게시글 검색 Fetch Join 쿼리문 구현

--- src/main/java/com/ssafy/ssafsound/domain/post/repository/PostRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/repository/PostRepository.java
@@ -17,7 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findWithDetailsByBoardId(@Param("boardId") Long boardId, Pageable pageable);
 
     boolean existsByIdAndMemberId(Long id, Long memberId);
-    
+
     @Query("SELECT p FROM post p JOIN FETCH p.member WHERE p.id = :id")
     Optional<Post> findByIdWithMember(@Param("id") Long id);
 
@@ -27,4 +27,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = {"board", "member"})
     List<Post> findWithDetailsByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
+    @Query("SELECT p FROM post p " +
+            "JOIN FETCH p.board b " +
+            "JOIN FETCH p.member " +
+            "WHERE TRIM(p.title) LIKE CONCAT('%', :keyword, '%') OR TRIM(p.content) LIKE CONCAT('%', :keyword, '%') " +
+            "AND b.id = :boardId ")
+    List<Post> findByBoardIdAndKeywordWithDetailsFetch(@Param("boardId") Long boardId, @Param("keyword") String keyword, Pageable pageable);
 }
