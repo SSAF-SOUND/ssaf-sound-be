87aadc23f9dde6bff7656c713fd63873e9c15dda
jjuny7712 <jjuny7712@gmail.com>
2023-07-06 16:49:45 +0900

    feat: 게시글 검색 관련 예외 메시지 추가

--- src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/exception/PostErrorInfo.java
@@ -9,7 +9,8 @@ public enum PostErrorInfo {
     DUPLICATE_REPORT("804", "이미 신고된 게시글입니다."),
     UNABLE_REPORT_MY_POST("805", "자신의 게시글은 신고할 수 없습니다."),
     UNAUTHORIZED_DELETE_POST("806", "해당 게시글 삭제 권한이 없습니다."),
-    UNAUTHORIZED_UPDATE_POST("807", "해당 게시글 수정 권한이 없습니다.");
+    UNAUTHORIZED_UPDATE_POST("807", "해당 게시글 수정 권한이 없습니다."),
+    NOT_FOUND_POSTS_SEARCH_RESULT("809", "검색 결과를 찾을 수 없습니다");
 
 
     private final String code;
