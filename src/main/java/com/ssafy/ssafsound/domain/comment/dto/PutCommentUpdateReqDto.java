9b2f7f40af6ea8ac21df1a87bdc7bcf7dd939e02
jjuny7712 <jjuny7712@gmail.com>
2023-07-07 10:45:10 +0900

    fix: 기본 생성자 어노테이션 추가

--- src/main/java/com/ssafy/ssafsound/domain/comment/dto/PutCommentUpdateReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/comment/dto/PutCommentUpdateReqDto.java
@@ -1,11 +1,12 @@
 package com.ssafy.ssafsound.domain.comment.dto;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Size;
 
 @Getter
+@NoArgsConstructor
 public class PutCommentUpdateReqDto {
     @Size(min = 2)
     private String content;
