95d394ae5c22990da3b0d8f0dbacfc63f4be7ab3
jjuny0310 <jjuny7712@gmail.com>
2023-07-10 00:13:20 +0900

    fix: 테스트 사용자 삭제

--- src/main/java/com/ssafy/ssafsound/domain/comment/controller/CommentController.java
+++ src/main/java/com/ssafy/ssafsound/domain/comment/controller/CommentController.java
@@ -1,11 +1,13 @@
 package com.ssafy.ssafsound.domain.comment.controller;
 
 import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
+import com.ssafy.ssafsound.domain.comment.dto.GetCommentResDto;
 import com.ssafy.ssafsound.domain.comment.dto.PostCommentWriteReqDto;
 import com.ssafy.ssafsound.domain.comment.service.CommentService;
 import com.ssafy.ssafsound.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,6 +26,14 @@ public class CommentController {
 
         return EnvelopeResponse.<Long>builder()
                 .data(commentService.writeComment(postId, authenticatedMember.getMemberId(), postCommentWriteReqDto))
+                .build();
+    }
+
+    @GetMapping
+    public EnvelopeResponse<GetCommentResDto> findComments(@RequestParam Long postId, Pageable pageable, AuthenticatedMember authenticatedMember) {
+
+        return EnvelopeResponse.<GetCommentResDto>builder()
+                .data(commentService.findComments(postId, authenticatedMember, pageable))
                 .build();
     }
 }
