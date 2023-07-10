b4ca1fbc2082503fed5b4d6107c7e03e3832986d
김태용 <amico741@naver.com>
2023-07-11 02:06:37 +0900

    remove: 사용하지 않는 repository 삭제

--- src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberLinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.ssafsound.domain.member.repository;
-
-import com.ssafy.ssafsound.domain.member.domain.MemberLink;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberLinkRepository extends JpaRepository<MemberLink, Long> {
-}
--- src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberSkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.ssafsound.domain.member.repository;
-
-import com.ssafy.ssafsound.domain.member.domain.MemberSkill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberSkillRepository extends JpaRepository<MemberSkill, Long> {
-}
