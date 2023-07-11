7c68ef341efc7f7a6687f85f8c11151eb9c38ceb
moonn6pence <shin421179@gmail.com>
2023-07-11 17:17:26 +0900

    test : 테스트 수정 및 컨버터 추가
    - CsvSource로 null 값 주입을 위해 NullableConverter 구현

--- src/test/java/com/ssafy/ssafsound/domain/lunch/service/LunchServiceTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/lunch/service/LunchServiceTest.java
@@ -12,25 +12,26 @@ import com.ssafy.ssafsound.domain.meta.domain.Campus;
 import com.ssafy.ssafsound.domain.meta.domain.MetaData;
 import com.ssafy.ssafsound.domain.meta.domain.MetaDataType;
 import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
+import com.ssafy.ssafsound.global.util.NullableConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class LunchServiceTest {
@@ -47,102 +48,84 @@ class LunchServiceTest {
 
     private Lunch lunch1;
     private Lunch lunch2;
-    private Lunch lunch3;
-    private Lunch lunch4;
     private Member member1;
     private Member member2;
-    private LunchPoll lunchPoll1;
-    private LunchPoll lunchPoll2;
-    private List<String> mainMenus;
+    private LunchPoll lunchPoll;
     private LocalDate today = LocalDate.now();
     private LocalDate tomorrow = today.plusDays(1);
-    private MetaData campus1;
-    private MetaData campus2;
+    private MetaData testCampus;
 
     @BeforeEach
     void setUp() {
-        mainMenus = new ArrayList<>();
 
-        // 테스트용 코스명 생성
-        for (int i = 0; i < 4; i++) {
-            mainMenus.add(String.format("mainMenu%d", i));
-        }
-
-        // 테스트용 캠퍼스 메타데이터 생성
-        campus1 = new MetaData(Campus.SEOUL);
-        campus2 = new MetaData(Campus.GUMI);
+        testCampus = new MetaData(Campus.SEOUL);
 
         lunch1 = Lunch.builder()
                 .id(1L)
-                .campus(campus1)
-                .mainMenu(mainMenus.get(0))
+                .campus(testCampus)
+                .mainMenu("mainMenu1")
                 .createdAt(today)
                 .build();
 
         lunch2 = Lunch.builder()
                 .id(2L)
-                .campus(campus1)
-                .mainMenu(mainMenus.get(1))
-                .createdAt(today)
-                .build();
-
-        lunch3 = Lunch.builder()
-                .id(3L)
-                .campus(campus1)
-                .mainMenu(mainMenus.get(2))
-                .createdAt(tomorrow)
-                .build();
-
-        lunch4 = Lunch.builder()
-                .id(4L)
-                .campus(campus2)
-                .mainMenu(mainMenus.get(3))
+                .campus(testCampus)
+                .mainMenu("mainMenu2")
                 .createdAt(today)
                 .build();
 
         member1 = Member.builder()
                 .id(1L)
                 .build();
+
         member2 = Member.builder()
                 .id(2L)
                 .build();
 
-        lunchPoll1 = LunchPoll.builder()
+        lunchPoll = LunchPoll.builder()
                 .lunch(lunch2)
                 .member(member1)
-                .polledAt(today)
-                .build();
-
-        lunchPoll2 = LunchPoll.builder()
-                .lunch(lunch2)
-                .member(member2)
                 .polledAt(today)
                 .build();
     }
 
     // 점심 메뉴 목록 조회 테스트
     @ParameterizedTest
-    @CsvSource({})
-    @DisplayName("캠퍼스와 당일 날짜로 점심 메뉴 목록을 조회한다.")
-    void Given_TodayDateAndCampus_When_FindLunches_Then_Succeed() {
+    @CsvSource({"1, 서울, 2023-07-10, 0", "2, 서울, 2023-07-10, -1", "null, 서울, 2023-07-10, -1"})
+    @DisplayName("캠퍼스와 날짜로 점심 메뉴 목록을 조회한다.")
+    void Given_TodayDateAndCampus_When_FindLunches_Then_Succeed(@ConvertWith(NullableConverter.class)Long memberId, String inputCampus, LocalDate inputDate,
+                                                                Long outputPolledAt) {
 
         // given
-        GetLunchListReqDto parameter = GetLunchListReqDto.builder().campus("서울").date(today).build();
-        given(metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), parameter.getCampus())).willReturn(campus1);
-        given(lunchRepository.findAllByCampusAndDate(campus1, today)).willReturn(Optional.of(List.of(lunch2, lunch1)));
+        given(metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), inputCampus))
+                .willReturn(new MetaData(Campus.SEOUL));
+
+        given(lunchRepository.findAllByCampusAndDate(metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), inputCampus), inputDate))
+                .willReturn(Optional.of(List.of(lunch2, lunch1)));
+
+        given(lunchPollRepository.findByMember_IdAndPolledAt(1L, inputDate))
+                .willReturn(lunchPoll);
+
+        given(lunchPollRepository.findByMember_IdAndPolledAt(2L, inputDate))
+                .willReturn(null);
+
+        GetLunchListReqDto getLunchListReqDto = GetLunchListReqDto.builder()
+                .campus(inputCampus)
+                .date(inputDate)
+                .build();
 
         // when
-        GetLunchListResDto result = lunchService.findLunches(null, parameter);
+        GetLunchListResDto result = lunchService.findLunches(memberId, getLunchListReqDto);
 
         // then
         assertAll(
                 () -> assertThat(result.getMenus().get(0))
                         .usingRecursiveComparison()
-                        .isEqualTo(GetLunchListElementResDto.of(lunch2,2L)),
+                        .isEqualTo(GetLunchListElementResDto.of(lunch2,1L)),
                 () -> assertThat(result.getMenus().get(1))
                         .usingRecursiveComparison()
-                        .isEqualTo(GetLunchListElementResDto.of(lunch1,2L)),
-                ()-> assertThat(result.getPolledAt()).isEqualTo(-1)
+                        .isEqualTo(GetLunchListElementResDto.of(lunch1,0L)),
+                ()-> assertThat(result.getPolledAt()).isEqualTo(outputPolledAt)
         );
     }
 
