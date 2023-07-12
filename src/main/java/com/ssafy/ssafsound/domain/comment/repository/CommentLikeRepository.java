f9ec48eb090544cd6f219976f06a11a14469ff93
jjuny7712 <jjuny7712@gmail.com>
2023-07-12 10:23:40 +0900

    feat: 댓글 좋아요 관련 Repository 생성

---
+++ src/main/java/com/ssafy/ssafsound/domain/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.ssafsound.domain.comment.repository;
+
+import com.ssafy.ssafsound.domain.comment.domain.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    Optional<CommentLike> findByCommentIdAndMemberId(Long commentId, Long memberId);
+}
