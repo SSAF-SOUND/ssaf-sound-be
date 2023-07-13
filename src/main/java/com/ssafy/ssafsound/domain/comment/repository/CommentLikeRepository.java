bebad31be924a72d761402650ae3ae6d2e1f90db
jjuny7712 <jjuny7712@gmail.com>
2023-07-14 00:01:43 +0900

    feat: 카운트 쿼리 추가

--- src/main/java/com/ssafy/ssafsound/domain/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/comment/repository/CommentLikeRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
     Optional<CommentLike> findByCommentIdAndMemberId(Long commentId, Long memberId);
+    Long countById(Long id);
 }
