805783b545e0c4d8d723c6f787f1998a4660ed51
Shin Hyeoncheol <65756225+moonn6pence@users.noreply.github.com>
2023-07-14 23:03:05 +0900

    test : 점심 메뉴 테스트 및 리팩토링
    * refact : qualifier 삭제
    
    - MetaDataConsumer의 @Qualifier 삭제
    
    * refact : 중복 프로퍼티 삭제
    
    * test : 테스트 코드 작성 시작
    
    * test : 점심 메뉴 목록 조회 성공 테스트 추가
    
    - 테스트 위해 req dto 빌더 패턴 생성자 추가
    - 메뉴 목록 조회 성공 테스트 추가. 추후에 ParameterizedTest 바꿀 가능성 존재.
    
    * test : 익일 점심 목록 조회 성공 테스트 추가
    
    - LunchService NPE 수정
    
    * test : 테스트용 인스턴스 수정
    
    - 인스턴스 대거 삭제
    - ParameterizedTest 도입
    
    * refactor : Lunch 도메인 리팩토링
    
    - LunchService의 LunchPoll 관련 로직 LunchPollService로 분리
    - Clock 객체를 받고, TimeConfig 컴포넌트를 등록해서 LocalDate.now() 처럼 사용하던 시간 관련 객체를 주입받는 형태로 변경
    
    * test : 테스트 수정 및 컨버터 추가
    
    - CsvSource로 null 값 주입을 위해 NullableConverter 구현
    
    * test : 테스트 코드 수정
    
    - 여러 케이스에 대해서는 lenient로 변경
    - verify 추가
    - clock의 getZone 도 stubbing
    
    * test : LunchServiceTest 추가 완료
    
    * refactor : memberId 관련 리팩토링
    
    - memberId로 Member 엔티티를 조회하는 방식으로 변경
    
    * test : 테스트 완성
    
    - Exception의 Info 까지 검증
    
    * test : 테스트 오류 수정 및 리팩토링
    
    - LunchErrorInfo 종류 수정, verify 추가
    
    * fix : 불필요한 빌더 삭제

--- src/main/java/com/ssafy/ssafsound/domain/lunch/service/LunchService.java
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/service/LunchService.java
@@ -7,13 +7,17 @@ import com.ssafy.ssafsound.domain.lunch.exception.LunchErrorInfo;
 import com.ssafy.ssafsound.domain.lunch.exception.LunchException;
 import com.ssafy.ssafsound.domain.lunch.repository.LunchPollRepository;
 import com.ssafy.ssafsound.domain.lunch.repository.LunchRepository;
+import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.member.exception.MemberErrorInfo;
+import com.ssafy.ssafsound.domain.member.exception.MemberException;
+import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
 import com.ssafy.ssafsound.domain.meta.domain.MetaData;
 import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,13 +30,18 @@ public class LunchService {
 
     private final LunchPollRepository lunchPollRepository;
 
-    @Qualifier("EnumMetaDataConsumer")
+    private final MemberRepository memberRepository;
+
     private final MetaDataConsumer metaDataConsumer;
+
+    private final Clock clock;
 
     @Transactional(readOnly = true)
     public GetLunchListResDto findLunches(Long memberId, GetLunchListReqDto getLunchListReqDto) {
 
-        Integer dayDifference = getLunchListReqDto.getDate().compareTo(LocalDate.now());
+        LocalDate currentTime = LocalDate.now(clock);
+
+        Integer dayDifference = getLunchListReqDto.getDate().compareTo(currentTime);
         if (dayDifference < 0 || dayDifference > 1) throw new LunchException(LunchErrorInfo.INVALID_DATE);
 
         MetaData campus = metaDataConsumer.getMetaData("CAMPUS",getLunchListReqDto.getCampus());
@@ -56,12 +65,16 @@ public class LunchService {
         }
 
         // 투표한 점심 메뉴 찾기
-        Lunch polledLunch = lunchPollRepository.findByMember_IdAndPolledAt(memberId, LocalDate.now()).getLunch();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorInfo.MEMBER_NOT_FOUND_BY_ID));
+
+        LunchPoll lunchPoll = lunchPollRepository.findByMemberAndPolledAt(member, currentTime);
+        Long polledAt = lunchPoll != null ? (long) lunches.indexOf(lunchPoll.getLunch()) : -1L;
 
         // 인증 유저 응답
         return GetLunchListResDto.of(lunches.stream()
                 .map(lunch -> GetLunchListElementResDto.of(lunch, (long)lunch.getLunchPolls().size()))
-                .collect(Collectors.toList()), (long) lunches.indexOf(polledLunch));
+                .collect(Collectors.toList()), polledAt);
     }
 
     @Transactional(readOnly = true)
@@ -69,56 +82,6 @@ public class LunchService {
 
         return GetLunchResDto.of(lunchRepository.findById(lunchId)
                 .orElseThrow(()->new LunchException(LunchErrorInfo.INVALID_LUNCH_ID)));
-    }
-
-    @Transactional
-    public PostLunchPollResDto saveLunchPoll(Long memberId, Long lunchId) {
-
-        // 1. 점심 메뉴 존재 여부 확인
-        Lunch lunch = lunchRepository.findById(lunchId)
-                .orElseThrow(()-> new LunchException(LunchErrorInfo.INVALID_LUNCH_ID));
-
-        // 2. 점심 메뉴가 당일 메뉴인지 확인
-        if (!lunch.getCreatedAt().isEqual(LocalDate.now())) {
-            throw new LunchException(LunchErrorInfo.INVALID_DATE);
-        }
-
-        // 3. 오늘 투표한 점심 투표 엔티티 조회
-        LunchPoll lunchPoll = lunchPollRepository.findByMember_IdAndPolledAt(memberId, LocalDate.now());
-
-        // 4-1. 이미 오늘 투표한 경우
-        if (lunchPoll != null) {
-            // 5-1. 중복 투표인 경우
-            if (lunchPoll.getLunch().equals(lunch)) {
-                throw new LunchException(LunchErrorInfo.DUPLICATE_LUNCH_POLL);
-            }
-
-            // 5-2. 투표 선택지가 바뀐 경우
-            lunchPoll.setLunch(lunch);
-        }
-
-        // 4-2. 오늘 첫 투표인 경우
-        else lunchPollRepository.saveByMember_IdAndLunch_Id(memberId, lunchId, LocalDate.now());
-
-        return PostLunchPollResDto.of((long) lunch.getLunchPolls().size());
-    }
-
-    @Transactional
-    public PostLunchPollResDto deleteLunchPoll(Long memberId, Long lunchId) {
-
-        // Lunch 엔티티 조회 후 validate
-        Lunch lunch = lunchRepository.findById(lunchId)
-                .orElseThrow(() -> new LunchException((LunchErrorInfo.INVALID_LUNCH_ID)));
-
-        // 멤버 id와 Lunch 엔티티 기반으로 LunchPoll 엔티티 조회 후 validate
-        LunchPoll lunchPoll = lunchPollRepository.findByMember_IdAndLunch(memberId, lunch)
-                .orElseThrow(() -> new LunchException(LunchErrorInfo.NO_LUNCH_POLL));
-
-        // 투표 삭제
-        lunchPollRepository.delete(lunchPoll);
-
-        // 변화한 투표 수 리턴
-        return PostLunchPollResDto.of((long) lunch.getLunchPolls().size());
     }
 
 }
--- src/test/java/com/ssafy/ssafsound/domain/lunch/service/LunchServiceTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/lunch/service/LunchServiceTest.java
@@ -1,62 +1,218 @@
 package com.ssafy.ssafsound.domain.lunch.service;
 
+import com.ssafy.ssafsound.domain.lunch.domain.Lunch;
+import com.ssafy.ssafsound.domain.lunch.domain.LunchPoll;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListElementResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListReqDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchResDto;
+import com.ssafy.ssafsound.domain.lunch.exception.LunchErrorInfo;
+import com.ssafy.ssafsound.domain.lunch.exception.LunchException;
+import com.ssafy.ssafsound.domain.lunch.repository.LunchPollRepository;
 import com.ssafy.ssafsound.domain.lunch.repository.LunchRepository;
+import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
+import com.ssafy.ssafsound.domain.meta.domain.Campus;
+import com.ssafy.ssafsound.domain.meta.domain.MetaData;
+import com.ssafy.ssafsound.domain.meta.domain.MetaDataType;
+import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
+import com.ssafy.ssafsound.global.util.NullableConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LunchServiceTest {
 
     @Mock
     private LunchRepository lunchRepository;
+    @Mock
+    private LunchPollRepository lunchPollRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private MetaDataConsumer metaDataConsumer;
+    @Mock
+    private Clock clock;
 
     @InjectMocks
     private LunchService lunchService;
 
+    private Lunch lunch1;
+    private Lunch lunch2;
+    private Member member1;
+    private Member member2;
+    private LunchPoll lunchPoll;
+    private String testLocalDateTime = "2023-07-10T10:15:00Z";
+    private String testLocalDate = "2023-07-10";
+    private LocalDate today = LocalDate.parse(testLocalDate);
+    private MetaData testCampus;
+
     @BeforeEach
     void setUp() {
 
+        testCampus = new MetaData(Campus.SEOUL);
+
+        lunch1 = Lunch.builder()
+                .id(1L)
+                .campus(testCampus)
+                .mainMenu("mainMenu1")
+                .createdAt(today)
+                .build();
+
+        lunch2 = Lunch.builder()
+                .id(2L)
+                .campus(testCampus)
+                .mainMenu("mainMenu2")
+                .createdAt(today)
+                .build();
+
+        member1 = Member.builder()
+                .id(1L)
+                .build();
+
+        member2 = Member.builder()
+                .id(2L)
+                .build();
+
+        lunchPoll = LunchPoll.builder()
+                .lunch(lunch2)
+                .member(member1)
+                .polledAt(today)
+                .build();
+
+        lenient().when(memberRepository.findById(1L)).thenReturn(Optional.of(member1));
+        lenient().when(memberRepository.findById(2L)).thenReturn(Optional.of(member2));
+
+        lenient().when(lunchPollRepository.findByMemberAndPolledAt(member1, today))
+                .thenReturn(lunchPoll);
+        lenient().when(lunchPollRepository.findByMemberAndPolledAt(member2, today))
+                .thenReturn(null);
+    }
+
+    // 점심 메뉴 목록 조회 테스트
+    @ParameterizedTest
+    @CsvSource({"1, 서울, 2023-07-10, 0", "2, 서울, 2023-07-10, -1", "null, 서울, 2023-07-10, -1"})
+    @DisplayName("멤버아이디, 캠퍼스, 날짜로 점심 메뉴 목록을 조회한다.")
+    void Given_DateAndCampus_When_FindLunches_Then_Succeed(@ConvertWith(NullableConverter.class)Long memberId, String inputCampus, LocalDate inputDate,
+                                                                Long outputPolledAt) {
+
+        // given
+        given(clock.instant()).willReturn(Instant.parse(testLocalDateTime));
+        given(clock.getZone()).willReturn(ZoneId.of("Asia/Seoul"));
+
+        given(metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), inputCampus))
+                .willReturn(new MetaData(Campus.SEOUL));
+
+        given(lunchRepository.findAllByCampusAndDate(metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), inputCampus), inputDate))
+                .willReturn(Optional.of(Arrays.asList(lunch2, lunch1)));
+
+        GetLunchListReqDto getLunchListReqDto = GetLunchListReqDto.builder()
+                .campus(inputCampus)
+                .date(inputDate)
+                .build();
+
+        // when
+        GetLunchListResDto result = lunchService.findLunches(memberId, getLunchListReqDto);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getMenus().get(0))
+                        .usingRecursiveComparison()
+                        .isEqualTo(GetLunchListElementResDto.of(lunch2,1L)),
+                () -> assertThat(result.getMenus().get(1))
+                        .usingRecursiveComparison()
+                        .isEqualTo(GetLunchListElementResDto.of(lunch1,0L)),
+                ()-> assertThat(result.getPolledAt()).isEqualTo(outputPolledAt)
+        );
+
+        verify(clock).instant();
+        verify(clock).getZone();
+        verify(metaDataConsumer, times(2)).getMetaData(MetaDataType.CAMPUS.name(), inputCampus);
+        verify(lunchRepository).findAllByCampusAndDate(
+                metaDataConsumer.getMetaData(MetaDataType.CAMPUS.name(), inputCampus), inputDate);
+
     }
 
     @Test
-    @DisplayName("정상 파라미터 요청시 점심 메뉴 목록 조회 성공")
-    void Given_ValidArgument_When_FindingAllLunchesBriefly_Then_Succeed() {
+    @DisplayName("유효하지 않은 날짜로 점심 메뉴 목록 조회시 예외를 던진다.")
+    void Given_InvalidDate_When_FindingAllLunches_Then_ThrowException() {
+
+        // given
+        given(clock.instant()).willReturn(Instant.parse(testLocalDateTime));
+        given(clock.getZone()).willReturn(ZoneId.of("Asia/Seoul"));
+
+        String inputCampus = "서울";
+        LocalDate inputDate = today.minusDays(1);
+        Long memberId = member1.getId();
+
+        GetLunchListReqDto getLunchListReqDto = GetLunchListReqDto.builder()
+                .campus(inputCampus)
+                .date(inputDate)
+                .build();
+
+        // when
+        // then
+        LunchException exception = assertThrows(LunchException.class, () -> lunchService.findLunches(memberId, getLunchListReqDto));
+        assertEquals(LunchErrorInfo.INVALID_DATE, exception.getInfo());
+
+        verify(clock).instant();
+        verify(clock).getZone();
+    }
+
+    // 점심 메뉴 상세 조회 테스트
+    @ParameterizedTest
+    @CsvSource({"1, mainMenu1","2, mainMenu2"})
+    @DisplayName("점심 아이디로 점심 상세 정보를 조회한다.")
+    void Given_ValidLunch_When_FindLunchDetail_Then_Succeed(Long lunchId, String outputMainMenu){
+
+        // given
+        lenient().when(lunchRepository.findById(1L)).thenReturn(Optional.of(lunch1));
+
+        lenient().when(lunchRepository.findById(2L)).thenReturn(Optional.of(lunch2));
+
+        // when
+        GetLunchResDto getLunchResDto = lunchService.findLunchDetail(lunchId);
+
+        // then
+        assertThat(getLunchResDto.getMainMenu()).isEqualTo(outputMainMenu);
     }
 
     @Test
-    @DisplayName("잘못된 타입의 파라미터 요청시 점심 메뉴 목록 조회 실패")
-    void Given_IllegalArgument_When_FindingAllLunchesBriefly_Then_ThrowException() {
-    }
+    @DisplayName("유효하지 않은 점심 아이디의 점심 메뉴 상세 조회 요청 시 예외를 던진다.")
+    void Given_InValidDateLunch_When_FindLunchDetail_Then_ThrowException(){
 
-    @Test
-    @DisplayName("유효하지 않은 캠퍼스 요청시 점심 메뉴 목록 조회 실패")
-    void Given_InvalidCampus_When_FindingAllLunchesBriefly_Then_ThrowException() {
-    }
+        // given
+        Long lunchId = 3L;
 
-    @Test
-    @DisplayName("유효하지 않은 날짜 요청시 점심 메뉴 목록 조회 성공")
-    void Given_InvalidDate_When_FindingAllLunchesBriefly_Then_ThrowException() {
-    }
+        given(lunchRepository.findById(3L)).willReturn(Optional.empty());
 
-    @Test
-    @DisplayName("정상 경로 요청시 점심 메뉴 상세 조회 성공")
-    void Given_ValidPathVariable_When_FindingALunchDetail_Then_Succeed(){
-    }
+        // when
+        // then
+        LunchException exception = assertThrows(LunchException.class, () -> lunchService.findLunchDetail(lunchId));
+        assertEquals(LunchErrorInfo.INVALID_LUNCH_ID, exception.getInfo());
 
-    @Test
-    @DisplayName("정상 점심 아이디 요청시 점심 메뉴 상세 조회 성공")
-    void Given_ValidId_When_FindingALunchDetail_Then_Succeed(){
-    }
-
-    @Test
-    @DisplayName("유효하지 않은 점심 아이디 요청시 점심 메뉴 상세 조회 실패")
-    void Given_InvalidId_When_FindingALunchDetail_Then_Succeed(){
+        verify(lunchRepository).findById(3L);
     }
 }
