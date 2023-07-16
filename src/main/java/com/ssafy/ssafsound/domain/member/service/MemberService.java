64eb765bd2c9aaf683a7b909c0cea3b0fe20d29e
김태용 <amico741@naver.com>
2023-07-16 15:13:46 +0900

    fix: 메서드들 순서 변경

--- src/main/java/com/ssafy/ssafsound/domain/member/service/MemberService.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/service/MemberService.java
@@ -113,33 +113,11 @@ public class MemberService {
         deleteExistMemberSkillsAllByMemberAndSaveNewRequest(member, putMemberProfileReqDto.getSkills());
     }
 
-    public void deleteExistMemberLinksAllByMemberAndSaveNewRequest(Member member, List<PutMemberLink> memberLinks) {
-        memberLinkRepository.deleteMemberLinksByMember(member);
-        member.setMemberLinks(memberLinks);
-    }
-
-    public void deleteExistMemberSkillsAllByMemberAndSaveNewRequest(Member member, List<String> memberSkills) {
-        memberSkillRepository.deleteMemberSkillsByMember(member);
-        member.setMemberSkills(memberSkills, metaDataConsumer);
-    }
-    public void setMemberPortfolioIntroduceByMember(Member member, PutMemberProfileReqDto putMemberProfileReqDto) {
-        if (putMemberProfileReqDto.getIntroduceMyself() != null) {
-            memberProfileRepository.findMemberProfileByMember(member).ifPresentOrElse(
-                    memberProfile -> memberProfile.changeIntroduceMyself(putMemberProfileReqDto.getIntroduceMyself()),
-                    () -> memberProfileRepository.save(putMemberProfileReqDto.toMemberProfile(member))
-            );
-        }
-    }
-
     @Transactional(readOnly = true)
     public MemberRole findMemberRoleByRoleName(String roleType) {
         return memberRoleRepository.findByRoleType(roleType).orElseThrow(() -> new MemberException(MemberErrorInfo.MEMBER_ROLE_TYPE_NOT_FOUND));
     }
 
-    public boolean isInvalidOauthLogin(Member member, PostMemberReqDto postMemberReqDto) {
-        OAuthType oAuthType = member.getOauthType();
-        return !member.getOauthIdentifier().equals(postMemberReqDto.getOauthIdentifier()) || !oAuthType.isEqual(postMemberReqDto.getOauthName());
-    }
     @Transactional(readOnly = true)
     public GetMemberResDto getMemberInformation(AuthenticatedMember authenticatedMember) {
         Member member = memberRepository.findById(authenticatedMember.getMemberId()).orElseThrow(() -> new MemberException(MemberErrorInfo.MEMBER_NOT_FOUND_BY_ID));
@@ -163,6 +141,29 @@ public class MemberService {
         } else {
             return PostNicknameResDto.of(true);
         }
+    }
+
+    public void deleteExistMemberLinksAllByMemberAndSaveNewRequest(Member member, List<PutMemberLink> memberLinks) {
+        memberLinkRepository.deleteMemberLinksByMember(member);
+        member.setMemberLinks(memberLinks);
+    }
+
+    public void deleteExistMemberSkillsAllByMemberAndSaveNewRequest(Member member, List<String> memberSkills) {
+        memberSkillRepository.deleteMemberSkillsByMember(member);
+        member.setMemberSkills(memberSkills, metaDataConsumer);
+    }
+    public void setMemberPortfolioIntroduceByMember(Member member, PutMemberProfileReqDto putMemberProfileReqDto) {
+        if (putMemberProfileReqDto.getIntroduceMyself() != null) {
+            memberProfileRepository.findMemberProfileByMember(member).ifPresentOrElse(
+                    memberProfile -> memberProfile.changeIntroduceMyself(putMemberProfileReqDto.getIntroduceMyself()),
+                    () -> memberProfileRepository.save(putMemberProfileReqDto.toMemberProfile(member))
+            );
+        }
+    }
+
+    public boolean isInvalidOauthLogin(Member member, PostMemberReqDto postMemberReqDto) {
+        OAuthType oAuthType = member.getOauthType();
+        return !member.getOauthIdentifier().equals(postMemberReqDto.getOauthIdentifier()) || !oAuthType.isEqual(postMemberReqDto.getOauthName());
     }
 
     private long getMinutesByDifferenceCertificationTryTime(LocalDateTime certificationTryTime) {
