38de59f2475b4c5654ed466659f4c96ddbd3cac3
김태용 <amico741@naver.com>
2023-07-16 15:45:20 +0900

    fix: outer join을 위한 엔티티 그래프로 변경

--- src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.member.repository;
 
 import com.ssafy.ssafsound.domain.member.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByOauthIdentifier(String oauthIdentifier);
     boolean existsByNickname(String nickname);
-    @Query("select m from member m join fetch m.memberLinks join fetch m.memberSkills where m.id = :memberId")
+    @EntityGraph(attributePaths = {"memberLinks", "memberSkills"})
+    @Query("select m from member m where m.id = :memberId")
     Optional<Member> findWithMemberLinksAndMemberSkills(@Param("memberId") Long memberId);
 }
