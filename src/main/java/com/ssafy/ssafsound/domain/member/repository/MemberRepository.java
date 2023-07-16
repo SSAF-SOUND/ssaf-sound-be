bbff0f406ccd6b7e7a6514ddc4d955ebc8f26ce3
김태용 <amico741@naver.com>
2023-07-16 15:19:28 +0900

    feat: 멤버의 링크들과 멤버 기술스택들을 조회하는 쿼리 메서드 추가

--- src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/repository/MemberRepository.java
@@ -2,10 +2,15 @@ package com.ssafy.ssafsound.domain.member.repository;
 
 import com.ssafy.ssafsound.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Optional<Member> findByOauthIdentifier(String oauthIdentifier);
     boolean existsByNickname(String nickname);
+    @Query("select m from member m join fetch m.memberLinks join fetch m.memberSkills where m.id = :memberId")
+    Optional<Member> findWithMemberLinksAndMemberSkills(@Param("memberId") Long memberId);
 }
