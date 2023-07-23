bc0bd348f4430af92ab8cc4243b164e90c72a680
jjuny7712 <jjuny7712@gmail.com>
2023-07-23 23:27:17 +0900

    feat: 나의 게시글 목록 조회 ReqDto 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostMyReqDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.ssafsound.domain.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+
+@Getter
+@Setter
+public class GetPostMyReqDto {
+    private Long cursor;
+
+    @Min(value = 10, message = "Size가 너무 작습니다.")
+    private int size;
+}
