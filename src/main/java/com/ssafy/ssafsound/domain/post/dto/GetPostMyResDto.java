2f90524e630ba947b495bbc4a2b74e997f661cff
jjuny7712 <jjuny7712@gmail.com>
2023-07-23 23:27:44 +0900

    fix: cursor 필드 추가

--- src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostMyResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostMyResDto.java
@@ -11,12 +11,20 @@ import java.util.stream.Collectors;
 @Builder
 public class GetPostMyResDto {
     private List<GetPostMyElement> posts;
+    private Long cursor;
 
-    public static GetPostMyResDto from(List<Post> posts) {
+    public static GetPostMyResDto of(List<Post> posts, int size) {
+        Long nextCursor = null;
+        if (posts.size() > size) {
+            posts = posts.subList(0, posts.size() - 1);
+            nextCursor = posts.get(posts.size() - 1).getId();
+        }
+
         return GetPostMyResDto.builder()
                 .posts(posts.stream()
                         .map(GetPostMyElement::from)
                         .collect(Collectors.toList()))
+                .cursor(nextCursor)
                 .build();
     }
 }
