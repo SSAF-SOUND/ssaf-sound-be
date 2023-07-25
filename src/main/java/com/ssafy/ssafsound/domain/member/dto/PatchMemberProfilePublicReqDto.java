eea406166dbef47f339845b80a43f7f83797de05
김태용 <amico741@naver.com>
2023-07-25 20:22:49 +0900

    feat: 멤버 프로필 공개여부 수정 DTO 추가

---
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/PatchMemberProfilePublicReqDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafsound.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PatchMemberProfilePublicReqDto {
+
+    private Boolean isPublic;
+}
