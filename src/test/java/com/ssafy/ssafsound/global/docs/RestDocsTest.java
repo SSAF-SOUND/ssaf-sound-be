885796c325b2ec703f10a3b4622672bb10448994
김태용 <amico741@naver.com>
2023-08-11 22:18:35 +0900

    fix: api 버저닝 제거

--- src/test/java/com/ssafy/ssafsound/global/docs/RestDocsTest.java
+++ src/test/java/com/ssafy/ssafsound/global/docs/RestDocsTest.java
@@ -23,7 +23,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -63,7 +62,6 @@ public class RestDocsTest {
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .alwaysDo(restDocs)
-                .alwaysDo(document("api/v1"))
                 .build();
     }
 }
