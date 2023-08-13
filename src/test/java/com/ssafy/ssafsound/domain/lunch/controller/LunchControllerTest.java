dc32fac20d36941f71d5e33aba6df101c48367e9
moonn6pence <shin421179@gmail.com>
2023-08-13 15:06:17 +0900

    test : lunch controller test field description

--- src/test/java/com/ssafy/ssafsound/domain/lunch/controller/LunchControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/lunch/controller/LunchControllerTest.java
@@ -1,16 +1,29 @@
 package com.ssafy.ssafsound.domain.lunch.controller;
 
+import com.ssafy.ssafsound.domain.auth.dto.AuthenticatedMember;
+import com.ssafy.ssafsound.domain.lunch.domain.Lunch;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListElementResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListReqDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchListResDto;
+import com.ssafy.ssafsound.domain.lunch.dto.GetLunchResDto;
+import com.ssafy.ssafsound.domain.lunch.service.LunchPollService;
+import com.ssafy.ssafsound.domain.lunch.service.LunchService;
 import com.ssafy.ssafsound.domain.meta.domain.Campus;
-import com.ssafy.ssafsound.utils.BaseRestDocControllerTest;
+import com.ssafy.ssafsound.domain.meta.domain.MetaData;
+import com.ssafy.ssafsound.global.docs.RestDocsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.restdocs.payload.FieldDescriptor;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.JsonFieldType;
 
+import javax.servlet.http.Cookie;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -19,45 +32,80 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class LunchControllerTest extends BaseRestDocControllerTest{
+@WebMvcTest(LunchController.class)
+public class LunchControllerTest extends RestDocsTest {
+
+    @MockBean
+    private LunchService lunchService;
+
+    @MockBean
+    private LunchPollService lunchPollService;
+
+    @MockBean
+    GetLunchListReqDto getLunchListReqDto;
 
     @Test
     @DisplayName("캠퍼스, 날짜로 점심 메뉴 목록 조회")
     public void getLunchesByCampusAndDate() throws Exception {
 
-        String today = LocalDate.now().toString();
-        String campus = Campus.SEOUL.getName();
+        // 요청
+        Cookie cookie = new Cookie("accessToken", "{accessToken}");
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("date", today);
-        params.add("campus", campus);
+        LocalDate requestParamDate = LocalDate.now();
+        String requestParamCampus = Campus.SEOUL.getName();
 
-        FieldDescriptor[] envelopFields = this.getEnvelopeResponseFields();
+        getLunchListReqDto = GetLunchListReqDto.builder()
+                .date(requestParamDate)
+                .campus(requestParamCampus)
+                .build();
 
-        this.mockMvc.perform(get("/lunch")
-                        .params(params))
+
+        // 응답
+        List<GetLunchListElementResDto> getLunchListElementResDtos = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            getLunchListElementResDtos.add(GetLunchListElementResDto.of(Lunch.builder()
+                    .id((long)i)
+                    .campus(new MetaData(Campus.SEOUL))
+                    .createdAt(requestParamDate)
+                    .mainMenu("메인메뉴" + i)
+                    .imagePath("www.sample" + i + ".png")
+                    .build(), (long) (30 - i * i * 2)));
+        }
+
+        GetLunchListResDto getLunchListResDto = GetLunchListResDto.of(getLunchListElementResDtos, 2L);
+
+        // mocking
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                .willReturn(AuthenticatedMember.builder().memberId(1L).build());
+
+        given(lunchService.findLunches(any(), any())).willReturn(getLunchListResDto);
+
+        // controller test
+        mockMvc.perform(get("/lunch?date=2023-08-12&campus=서울")
+                        .cookie(cookie)
+                        .characterEncoding("UTF-8"))
                 .andExpect(status().isOk())
                 .andDo(
                         restDocs.document(
                                 requestParameters(
-                                        parameterWithName("date").description("조회하려는 점심 메뉴의 yyyy-MM-DD 형식 날짜. 당일과 익일만 가능합니다."),
-                                        parameterWithName("campus").description("조회하려는 점심 메뉴의 캠퍼스 한글명. (서울 | 부울경 | 구미 | 광주")
+                                        parameterWithName("date").description("조회하려는 yyyy-MM-DD 형식 일자. 당일과 익일만 가능합니다."),
+                                        parameterWithName("campus").description("조회하려는 캠퍼스 한글명. (서울 | 부울경 | 구미 | 광주 )( 대전은 현재 미지원 )")
                                 ),
                                 responseFields(
-                                        envelopFields[0],
-                                        envelopFields[1],
-                                        envelopFields[2],
-                                        fieldWithPath("data.totalPollCount").description("전체 메뉴의 투표수 합계"),
-                                        fieldWithPath("data.polledAt").description("점심 메뉴 목록에서 인증된 사용자의 투표 선택지 인덱스. 미인증 혹은 투표하지 않았을 경우 -1"),
-                                        fieldWithPath("data.menus").description("점심 메뉴 목록")
+                                        fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+                                        fieldWithPath("data.totalPollCount").type(JsonFieldType.NUMBER).description("전체 메뉴의 투표수 합계"),
+                                        fieldWithPath("data.polledAt").type(JsonFieldType.NUMBER).description("점심 메뉴 목록에서 인증된 사용자의 투표 선택지 인덱스. 미인증 혹은 투표하지 않았을 경우 -1"),
+                                        fieldWithPath("data.menus").type(JsonFieldType.ARRAY).description("점심 메뉴 목록")
                                 ).andWithPrefix("data.menus[]",
-                                        fieldWithPath("mainMenu").description("메인 메뉴명"),
-                                        fieldWithPath("imagePath").description("메인 메뉴 이미지 url"),
-                                        fieldWithPath("pollCount").description("메뉴 투표수")
+                                        fieldWithPath("mainMenu").type(JsonFieldType.STRING).description("메인 메뉴명"),
+                                        fieldWithPath("imagePath").type(JsonFieldType.STRING).description("메인 메뉴 이미지 url"),
+                                        fieldWithPath("pollCount").type(JsonFieldType.NUMBER).description("메뉴 투표수")
                                 )
                         )
                 );
-
     }
 
     @Test
@@ -65,6 +113,15 @@ public class LunchControllerTest extends BaseRestDocControllerTest{
     public void getLunchByLunchId() throws Exception {
 
         Long sampleLunchId = 1L;
+
+        Lunch lunch = Lunch.builder()
+                .id(sampleLunchId)
+                .mainMenu("메인 메뉴1")
+                .extraMenu("메인 메뉴1, 엑스트라 메뉴1, 엑스트라 메뉴2")
+                .sumKcal("2,321 kcal")
+                .build();
+
+        given(lunchService.findLunchDetail(sampleLunchId)).willReturn(GetLunchResDto.of(lunch));
 
         this.mockMvc.perform(get("/lunch/{lunchId}", sampleLunchId))
                 .andExpect(status().isOk())
@@ -74,17 +131,15 @@ public class LunchControllerTest extends BaseRestDocControllerTest{
                                         parameterWithName("lunchId").description("점심 메뉴 아이디")
                                 ),
                                 responseFields(
-                                        fieldWithPath("code").description("응답 코드"),
-                                        fieldWithPath("message").description("응답 메시지"),
-                                        fieldWithPath("data").description("응답 데이터"),
-                                        fieldWithPath("data.mainMenu").description("메인 메뉴명"),
-                                        fieldWithPath("data.extraMenu").description("전체 메뉴명"),
-                                        fieldWithPath("data.sumKcal").description("총 칼로리")
+                                        fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+                                        fieldWithPath("data.mainMenu").type(JsonFieldType.STRING).description("메인 메뉴명"),
+                                        fieldWithPath("data.extraMenu").type(JsonFieldType.STRING).description("전체 메뉴명"),
+                                        fieldWithPath("data.sumKcal").type(JsonFieldType.STRING).description("총 칼로리")
                                 )
                         )
                 );
     }
-
-//    @Test
 
 }
