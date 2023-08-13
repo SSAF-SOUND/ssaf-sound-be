565746e21872a43075b6dd4ef22cf9b9b7e88fed
김태용 <amico741@naver.com>
2023-08-14 01:57:27 +0900

    refactor: 멤버의 포트폴리오 조회 시, 공개 여부 필드 삭제

--- src/main/java/com/ssafy/ssafsound/domain/member/dto/GetMemberPortfolioResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/GetMemberPortfolioResDto.java
@@ -13,25 +13,20 @@ import lombok.NoArgsConstructor;
 @Getter
 public class GetMemberPortfolioResDto {
 
-    private Boolean isPublic;
-
     private PortfolioElement portfolioElement;
 
     public static GetMemberPortfolioResDto of(Member member, MemberProfile memberProfile) {
         if(member.getPublicProfile()) {
             return GetMemberPortfolioResDto.builder()
-                    .isPublic(true)
                     .portfolioElement(PortfolioElement.of(member, memberProfile))
                     .build();
         }
         return GetMemberPortfolioResDto.builder()
-                .isPublic(false)
                 .build();
     }
 
     public static GetMemberPortfolioResDto ofMyPortfolio(Member member, MemberProfile memberProfile) {
         return GetMemberPortfolioResDto.builder()
-                .isPublic(member.getPublicProfile())
                 .portfolioElement(PortfolioElement.of(member, memberProfile))
                 .build();
     }
