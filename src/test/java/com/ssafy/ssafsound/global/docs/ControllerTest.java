3ab7256df043cbd096cfd3a467fa810027b81108
TAEYONG KIM <86272688+YongsHub@users.noreply.github.com>
2023-08-13 18:41:39 +0900

    test: AuthControllerTest Rest Docs With Rest Assured
    * refactor: controller test 형식 변경으로 인한 제거
    
    * feat: AuthFixture 리프레시토큰 쿠키 반환 제거 및 dto 생성 메서드 추가
    
    * feat: ControllerTest에 ResponseFieldsSnippet 생성 및 쿠키 추가
    
    * feat: AuthControllerTest RestAssured로 리팩토링
    
    * chore: rest-assured 다운그레이드
    
    * feat: ControllerTest 공통으로 사용할 쿠키 및 Envelop Pattern 메서드화
    
    * refactor: AuthControllerTest 리팩토링
    
    * feat: host name 및 포트번호 추가
    
    * docs: index 및 auth docs 수정
    
    * fix: 데이터 존재 여부에 대해 리팩토링
    
    * feat: Member Fixture 추가

--- src/test/java/com/ssafy/ssafsound/global/docs/ControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/global/docs/ControllerTest.java
@@ -1,39 +1,60 @@
 package com.ssafy.ssafsound.global.docs;
 
 import com.ssafy.ssafsound.domain.auth.controller.AuthController;
+import com.ssafy.ssafsound.domain.auth.service.AuthService;
+import com.ssafy.ssafsound.domain.auth.service.CookieProvider;
 import com.ssafy.ssafsound.domain.auth.service.token.JwtTokenProvider;
-import com.ssafy.ssafsound.domain.auth.validator.AuthenticationArgumentResolver;
 import com.ssafy.ssafsound.domain.board.controller.BoardController;
-import com.ssafy.ssafsound.domain.chat.controller.ChatController;
+import com.ssafy.ssafsound.domain.board.service.BoardService;
 import com.ssafy.ssafsound.domain.comment.controller.CommentController;
+import com.ssafy.ssafsound.domain.comment.service.CommentService;
 import com.ssafy.ssafsound.domain.lunch.controller.LunchController;
+import com.ssafy.ssafsound.domain.lunch.service.LunchPollService;
+import com.ssafy.ssafsound.domain.lunch.service.LunchService;
 import com.ssafy.ssafsound.domain.member.controller.MemberController;
+import com.ssafy.ssafsound.domain.member.service.MemberService;
 import com.ssafy.ssafsound.domain.meta.controller.MetaDataController;
+import com.ssafy.ssafsound.domain.meta.service.EnumMetaDataConsumer;
 import com.ssafy.ssafsound.domain.post.controller.PostController;
+import com.ssafy.ssafsound.domain.post.service.PostService;
 import com.ssafy.ssafsound.domain.recruit.controller.RecruitController;
+import com.ssafy.ssafsound.domain.recruit.service.RecruitService;
 import com.ssafy.ssafsound.domain.recruitapplication.controller.RecruitApplicationController;
+import com.ssafy.ssafsound.domain.recruitapplication.service.RecruitApplicationService;
 import com.ssafy.ssafsound.domain.recruitcomment.controller.RecruitCommentController;
+import com.ssafy.ssafsound.domain.recruitcomment.service.RecruitCommentService;
 import com.ssafy.ssafsound.global.interceptor.AuthInterceptor;
+import com.ssafy.ssafsound.infra.storage.controller.StorageController;
+import com.ssafy.ssafsound.infra.storage.service.AwsS3StorageService;
+import io.restassured.http.Cookie;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.headers.RequestHeadersSnippet;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
 
 @WebMvcTest({
         AuthController.class,
         BoardController.class,
-        ChatController.class,
         CommentController.class,
         LunchController.class,
         MemberController.class,
@@ -41,7 +62,9 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
         PostController.class,
         RecruitController.class,
         RecruitApplicationController.class,
-        RecruitCommentController.class
+        RecruitCommentController.class,
+        StorageController.class
+        //ChatController.class
 })
 @ExtendWith(RestDocumentationExtension.class)
 public class ControllerTest {
@@ -49,20 +72,74 @@ public class ControllerTest {
     protected MockMvcRequestSpecification restDocs;
 
     @MockBean
-    JwtTokenProvider jwtTokenProvider;
+    protected MemberService memberService;
+
+    @SpyBean
+    protected CookieProvider cookieProvider;
 
     @MockBean
-    protected AuthenticationArgumentResolver authenticationArgumentResolver;
+    protected AuthService authService;
+
+    @MockBean
+    protected BoardService boardService;
+
+    @MockBean
+    protected CommentService commentService;
+
+    @MockBean
+    protected LunchService lunchService;
+
+    @MockBean
+    protected LunchPollService lunchPollService;
+
+    @MockBean
+    protected EnumMetaDataConsumer enumMetaDataConsumer;
+
+    @MockBean
+    protected PostService postService;
+
+    @MockBean
+    protected RecruitService recruitService;
+
+    @MockBean
+    protected RecruitApplicationService recruitApplicationService;
+
+    @MockBean
+    protected RecruitCommentService recruitCommentService;
+
+    @MockBean
+    protected AwsS3StorageService awsS3StorageService;
+
+    @MockBean
+    protected JwtTokenProvider jwtTokenProvider;
 
     @MockBean
     protected AuthInterceptor authInterceptor;
 
+    protected static final Cookie ACCESS_TOKEN = new Cookie.Builder("accessToken", "accessTokenValue").build();
+    protected static final Cookie REFRESH_TOKEN = new Cookie.Builder("refreshToken", "refreshTokenValue").build();
+
+    private final ResponseFieldsSnippet envelopPatternWithData = responseFields(
+            fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+            fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+            fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"));
+
+
+    private final ResponseFieldsSnippet envelopPatternWithNoContent = responseFields(
+            fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+            fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+            fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터").optional());
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         restDocs = RestAssuredMockMvc.given()
                 .mockMvc(MockMvcBuilders.webAppContextSetup(webApplicationContext)
                         .apply(documentationConfiguration(restDocumentation)
+                                .uris()
+                                .withScheme("https")
+                                .withHost("api.ssafsound.com")
+                                .withPort(443)
+                                .and()
                                 .operationPreprocessors()
                                 .withRequestDefaults(prettyPrint())
                                 .withResponseDefaults(prettyPrint()))
@@ -72,5 +149,27 @@ public class ControllerTest {
         doReturn(true)
                 .when(authInterceptor)
                 .preHandle(any(), any(), any());
+    }
+
+    protected static final RequestHeadersSnippet accessTokenPattern = requestHeaders(
+            headerWithName("Cookie").description("액세스 토큰"));
+
+    protected static final RequestHeadersSnippet accessTokenPatternOptional = requestHeaders(
+            headerWithName("Cookie").description("액세스 토큰").optional());
+
+    protected RequestHeadersSnippet getAccessTokenPattern() {
+        return accessTokenPattern;
+    }
+
+    protected RequestHeadersSnippet getAccessTokenOptionalPattern() {
+        return accessTokenPatternOptional;
+    }
+
+    protected ResponseFieldsSnippet getEnvelopPatternWithData() {
+        return envelopPatternWithData;
+    }
+
+    protected ResponseFieldsSnippet getEnvelopPatternWithNoContent() {
+        return envelopPatternWithNoContent;
     }
 }
