d173da275df7e24b27494ffc093b92d730119a12
jjuny7712 <jjuny7712@gmail.com>
2023-08-14 19:31:53 +0900

    test: board 컨트롤러 테스트 구현

--- src/test/java/com/ssafy/ssafsound/domain/board/controller/BoardControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/board/controller/BoardControllerTest.java
@@ -1,37 +1,52 @@
 package com.ssafy.ssafsound.domain.board.controller;
 
-import com.ssafy.ssafsound.utils.BaseRestDocControllerTest;
+import com.ssafy.ssafsound.domain.board.dto.GetBoardResDto;
+import com.ssafy.ssafsound.global.docs.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-class BoardControllerTest extends BaseRestDocControllerTest {
+class BoardControllerTest extends ControllerTest {
     @Test
-    @DisplayName("게시판 조회")
-    public void findBoards() throws Exception {
-        mockMvc.perform(get("/boards")
-                        .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().isOk())
-                .andDo(
-                        restDocs.document(
-                                responseFields(
-                                        fieldWithPath("code").description("응답 코드"),
-                                        fieldWithPath("message").description("응답 메시지"),
-                                        fieldWithPath("data").description("응답 데이터"),
-                                        fieldWithPath("data.boards").description("게시판 목록 데이터")
-                                ).andWithPrefix("data.boards[].",
-                                        fieldWithPath("boardId").description("게시판의 id"),
-                                        fieldWithPath("title").description("게시판 이름"),
-                                        fieldWithPath("imageUrl").description("게시판 이미지"),
-                                        fieldWithPath("description").description("게시판 설명")
-                                )
-                        )
-                );
+    @DisplayName("게시판 목록 조회 데이터 반환")
+    public void findBoards() {
+//        doReturn(GetBoardResDto.of)
+//                .when(boardService)
+//                .findBoards();
+
+        restDocs
+                .when().get("/boards")
+                .then().log().all()
+                .apply(document("board/find-boards"))
+                .statusCode(HttpStatus.OK.value());
+
+//        mockMvc.perform(get("/boards")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                )
+//                .andExpect(status().isOk())
+//                .andDo(
+//                        restDocs.document(
+//                                responseFields(
+//                                        fieldWithPath("code").description("응답 코드"),
+//                                        fieldWithPath("message").description("응답 메시지"),
+//                                        fieldWithPath("data").description("응답 데이터"),
+//                                        fieldWithPath("data.boards").description("게시판 목록 데이터")
+//                                ).andWithPrefix("data.boards[].",
+//                                        fieldWithPath("boardId").description("게시판의 id"),
+//                                        fieldWithPath("title").description("게시판 이름"),
+//                                        fieldWithPath("imageUrl").description("게시판 이미지"),
+//                                        fieldWithPath("description").description("게시판 설명")
+//                                )
+//                        )
+//                );
     }
 }
