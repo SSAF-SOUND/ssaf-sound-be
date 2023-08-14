ede716b361d337e3f0d5db440f6e3ca4e04a4623
jjuny7712 <jjuny7712@gmail.com>
2023-08-14 20:49:42 +0900

    test: Post 관련 Fixture 생성

---
+++ src/test/java/com/ssafy/ssafsound/global/util/fixture/PostFixture.java
@@ -0,0 +1,39 @@
+package com.ssafy.ssafsound.global.util.fixture;
+
+import com.ssafy.ssafsound.domain.board.dto.GetBoardResDto;
+import com.ssafy.ssafsound.domain.post.domain.Post;
+import com.ssafy.ssafsound.domain.post.domain.PostLike;
+import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
+
+import java.util.List;
+
+import static com.ssafy.ssafsound.global.util.fixture.BoardFixture.BOARD_FIXTURE1;
+import static com.ssafy.ssafsound.global.util.fixture.MemberFixture.MEMBER_SHERYL;
+import static com.ssafy.ssafsound.global.util.fixture.MemberFixture.MEMBER_WALTER;
+
+public class PostFixture {
+
+    public static final Integer PAGE_SIZE1 = 10;
+
+    public static final Post POST_FIXTURE1 = Post.builder()
+            .id(1L)
+            .title("싸탈하고 싶다.")
+            .content("싸탈하고 싶은 밤이네요")
+            .anonymity(true)
+            .board(BOARD_FIXTURE1)
+            .member(MEMBER_WALTER)
+            .build();
+
+    public static final Post POST_FIXTURE2 = Post.builder()
+            .id(2L)
+            .title("삼성 B형을 봤는데")
+            .content("결과가 암울해")
+            .anonymity(false)
+            .board(BOARD_FIXTURE1)
+            .member(MEMBER_SHERYL)
+            .build();
+
+    public static final GetPostResDto GET_POST_RES_DTO1 = GetPostResDto.of(
+            List.of(POST_FIXTURE1,
+                    POST_FIXTURE2), PAGE_SIZE1);
+}
