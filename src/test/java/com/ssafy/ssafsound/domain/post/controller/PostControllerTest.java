ba03cf066fe1c0f357ad24dfd0e5409ceda7a7a5
jjuny7712 <jjuny7712@gmail.com>
2023-08-15 23:47:13 +0900

    test: Hot 게시글 목록 조회 컨트롤러 테스트 구현

--- src/test/java/com/ssafy/ssafsound/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/post/controller/PostControllerTest.java
@@ -20,7 +20,7 @@ class PostControllerTest extends ControllerTest {
     @Test
     @DisplayName("게시글 목록 조회, cursor와 size를 기준으로 커서기반 페이지네이션이 수행됨.")
     void findPosts() {
-        doReturn(GET_POST_RES_DTO1)
+        doReturn(GET_POST_RES_DTO3)
                 .when(postService)
                 .findPosts(any());
 
@@ -293,14 +293,47 @@ class PostControllerTest extends ControllerTest {
                                         fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("s3에 저장된 이미지를 CDN URL 주소로 가져오기 위한 필드, 해당 URL에 접근만 해도 이미지에 접근할 수 있는 경로를 의미").optional()
                                 ),
                                 getEnvelopPatternWithData().andWithPrefix("data.",
-                                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("수젖ㅇ한 게시글 ID")
+                                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("수정한 게시글 ID")
                                 )
                         )
                 );
     }
 
     @Test
+    @DisplayName("Hot 게시글 목록 조회")
     void findHotPosts() {
+        doReturn(GET_POST_HOT_RES_DTO1)
+                .when(postService)
+                .findHotPosts(any());
+
+        restDocs.cookie(ACCESS_TOKEN)
+                .when().get("/posts/hot?cursor={cursor}&size={pageSize}", -1, 10)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .apply(document("post/find-hot-posts",
+                                requestCookieAccessTokenNeedless(),
+                                requestParameters(
+                                        parameterWithName("cursor").description("cursor값은 다음 페이지를 가져올 마지막 페이지 번호를 의미함, 초기 cursor는 -1, 이후 cursor값은 응답 데이터로 제공되는 cursor값을 사용."),
+                                        parameterWithName("size").description("cursor를 기준으로 다음에 불러올 페이지의 size를 의미, 최소 size는 10")
+                                ),
+                                getEnvelopPatternWithData().andWithPrefix("data.",
+                                        fieldWithPath("posts").type(JsonFieldType.ARRAY).description("게시글 목록"),
+                                        fieldWithPath("cursor").type(JsonFieldType.NUMBER).description("다음에 요청할 cursor값, 응답되는 cursor값이 null이면 다음 페이지는 없음을 의미").optional()
+                                ).andWithPrefix("data.posts[].",
+                                        fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시글이 작성된 게시판 종류의 ID"),
+                                        fieldWithPath("boardTitle").type(JsonFieldType.STRING).description("게시글이 작성된 게시판의 종류, 자유 게시판 | 취업 게시판 | 맛집 게시판 | 질문 게시판 | 싸피 예비생 게시판"),
+                                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글의 고유 ID"),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("게시글의 제목"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("게시글의 내용"),
+                                        fieldWithPath("likeCount").type(JsonFieldType.NUMBER).description("게시글의 좋아요 개수"),
+                                        fieldWithPath("commentCount").type(JsonFieldType.NUMBER).description("게시글의 댓글 개수"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("게시글의 작성일").optional(),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("게시글 작성자의 닉네임"),
+                                        fieldWithPath("anonymity").type(JsonFieldType.BOOLEAN).description("게시글 작성자의 익명 여부인지 나타내는 필드"),
+                                        fieldWithPath("thumbnail").type(JsonFieldType.STRING).description("게시판의 썸네일, 게시글의 사진이 여러개가 있을 때 첫 번째 사진이 해당 게시글의 썸네일이 됨.").optional()
+                                )
+                        )
+                );
     }
 
     @Test
@@ -310,7 +343,7 @@ class PostControllerTest extends ControllerTest {
     @Test
     @DisplayName("게시글 검색(제목 + 내용을 기준으로 검색)")
     void searchPosts() {
-        doReturn(GET_POST_RES_DTO2)
+        doReturn(GET_POST_RES_DTO4)
                 .when(postService)
                 .searchPosts(any());
 
