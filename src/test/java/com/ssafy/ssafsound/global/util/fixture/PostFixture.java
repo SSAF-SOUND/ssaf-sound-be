bb57386d661526a642181e4f5d2a8e86fb12c377
jjuny7712 <jjuny7712@gmail.com>
2023-08-15 23:23:29 +0900

    test: 게시글 검색 컨트롤러 테스트 구현

--- src/test/java/com/ssafy/ssafsound/global/util/fixture/PostFixture.java
+++ src/test/java/com/ssafy/ssafsound/global/util/fixture/PostFixture.java
@@ -6,6 +6,7 @@ import com.ssafy.ssafsound.domain.post.dto.*;
 import java.util.List;
 
 import static com.ssafy.ssafsound.global.util.fixture.BoardFixture.BOARD_FIXTURE1;
+import static com.ssafy.ssafsound.global.util.fixture.BoardFixture.BOARD_FIXTURE2;
 import static com.ssafy.ssafsound.global.util.fixture.MemberFixture.*;
 
 public class PostFixture {
@@ -28,9 +29,22 @@ public class PostFixture {
             .member(MEMBER_YONG)
             .build();
 
+    public static final Post POST_FIXTURE3 = Post.builder()
+            .id(3L)
+            .title("안녕하세요 취업 게시판의 첫 글이네요")
+            .content("취업이 뭐죠??")
+            .anonymity(true)
+            .board(BOARD_FIXTURE2)
+            .member(MEMBER_YONG)
+            .build();
+
     public static final GetPostResDto GET_POST_RES_DTO1 = GetPostResDto.of(
             List.of(POST_FIXTURE1,
                     POST_FIXTURE2), 10);
+
+    public static final GetPostResDto GET_POST_RES_DTO2 = GetPostResDto.of(
+            List.of(POST_FIXTURE3), 10);
+
 
     public static final GetPostDetailResDto GET_POST_DETAIL_RES_DTO1 = GetPostDetailResDto.of(
             POST_FIXTURE1, null);
