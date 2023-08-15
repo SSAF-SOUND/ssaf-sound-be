12b725c414dc8a7f9157654b73ee05deffda38b5
TAEYONG KIM <86272688+YongsHub@users.noreply.github.com>
2023-08-15 23:22:17 +0900

    refactor: 마이 프로필 조회 관련 기획 변경으로 인한 리팩토링
    * refactor: 멤버의 포트폴리오 조회 시, 공개 여부 필드 삭제
    
    * refactor: 멤버의 프로필 공개 여부가 false인데 조회하는 경우 처리 추가
    
    * feat: 다른 멤버의 프로필 공개여부 조회 기능 추가

--- src/main/java/com/ssafy/ssafsound/domain/member/dto/GetMemberPortfolioResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/member/dto/GetMemberPortfolioResDto.java
@@ -13,25 +13,10 @@ import lombok.NoArgsConstructor;
 @Getter
 public class GetMemberPortfolioResDto {
 
-    private Boolean isPublic;
-
     private PortfolioElement portfolioElement;
 
-    public static GetMemberPortfolioResDto of(Member member, MemberProfile memberProfile) {
-        if(member.getPublicProfile()) {
-            return GetMemberPortfolioResDto.builder()
-                    .isPublic(true)
-                    .portfolioElement(PortfolioElement.of(member, memberProfile))
-                    .build();
-        }
+    public static GetMemberPortfolioResDto ofMemberProfile(Member member, MemberProfile memberProfile) {
         return GetMemberPortfolioResDto.builder()
-                .isPublic(false)
-                .build();
-    }
-
-    public static GetMemberPortfolioResDto ofMyPortfolio(Member member, MemberProfile memberProfile) {
-        return GetMemberPortfolioResDto.builder()
-                .isPublic(member.getPublicProfile())
                 .portfolioElement(PortfolioElement.of(member, memberProfile))
                 .build();
     }
