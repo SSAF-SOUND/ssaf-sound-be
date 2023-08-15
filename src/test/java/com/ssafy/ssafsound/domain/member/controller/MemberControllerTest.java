404273204d91da711487fa0e11d683f82791afd8
김태용 <amico741@naver.com>
2023-08-15 19:16:15 +0900

    test: 회원가입 플로우에서 사용자 정보 입력 테스트 추가

--- src/test/java/com/ssafy/ssafsound/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/member/controller/MemberControllerTest.java
@@ -1,10 +1,12 @@
 package com.ssafy.ssafsound.domain.member.controller;
 
 import com.ssafy.ssafsound.domain.member.dto.GetMemberResDto;
+import com.ssafy.ssafsound.domain.member.dto.PostMemberInfoReqDto;
 import com.ssafy.ssafsound.global.docs.ControllerTest;
 import com.ssafy.ssafsound.global.util.fixture.MemberFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import static com.ssafy.ssafsound.global.docs.snippet.CookieDescriptionSnippet.requestCookieAccessTokenMandatory;
@@ -75,7 +77,7 @@ class MemberControllerTest extends ControllerTest {
     void getSSAFYMemberInformation() {
 
         given(memberService.getMemberInformation(any()))
-                .willReturn(MemberFixture.SSAFY_MEMBER_RESPONSE_DTO);
+                .willReturn(MemberFixture.CERTIFIED_SSAFY_MEMBER);
 
         restDocs
                 .cookie(ACCESS_TOKEN)
@@ -105,6 +107,91 @@ class MemberControllerTest extends ControllerTest {
                                         fieldWithPath("certificationState").type(JsonFieldType.STRING)
                                                 .description("싸피생 인증상태(CERTIFIED/UNCERTIFIED)"),
                                         fieldWithPath("majorTrack").type(JsonFieldType.STRING)
+                                                .description("전공 트랙(인증상태가 UNCERTIFIED라면 NULL)"))))
+                .expect(status().isOk());
+    }
+
+    @DisplayName("사용자 회원가입 플로우에서 기본 정보 입력에 성공한다.")
+    @Test
+    void putMemberInformation() {
+
+        given(memberService.registerMemberInformation(any(), any()))
+                .willReturn(GetMemberResDto.fromGeneralUser(MemberFixture.GENERAL_MEMBER));
+        PostMemberInfoReqDto postMemberInfoReqDto = PostMemberInfoReqDto.builder()
+                .nickname("james")
+                .ssafyMember(false)
+                .isMajor(true)
+                .build();
+
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .cookie(ACCESS_TOKEN)
+                .body(postMemberInfoReqDto)
+                .when().put("/members")
+                .then().log().all()
+                .assertThat()
+                .apply(document("members/put-information",
+                        requestCookieAccessTokenMandatory(),
+                        getEnvelopPatternWithData()
+                                .and(fieldWithPath("data.memberId").type(JsonFieldType.NUMBER)
+                                        .description("멤버 ID"))
+                                .and(fieldWithPath("data.memberRole").type(JsonFieldType.STRING)
+                                        .description("멤버 권한"))
+                                .and(fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                        .description("닉네임"))
+                                .and(fieldWithPath("data.ssafyMember").type(JsonFieldType.BOOLEAN)
+                                        .description("싸피인 여부 FALSE"))
+                                .and(fieldWithPath("data.isMajor").type(JsonFieldType.BOOLEAN)
+                                        .description("전공자 여부 TRUE OR FALSE"))
+                                .and(fieldWithPath("data.ssafyInfo").type(JsonFieldType.NULL)
+                                        .description("싸피인 정보 NULL"))))
+                .expect(status().isOk());
+    }
+
+    @DisplayName("사용자 회원가입 플로우에서 싸피 정보 입력에 성공한다.")
+    @Test
+    void putMemberSSAFYInformation() {
+
+        given(memberService.registerMemberInformation(any(), any()))
+                .willReturn(MemberFixture.UNCERTIFIED_SSAFY_MEMBER);
+        PostMemberInfoReqDto postMemberInfoReqDto = PostMemberInfoReqDto.builder()
+                .nickname("james")
+                .ssafyMember(true)
+                .isMajor(true)
+                .semester(9)
+                .campus("서울")
+                .build();
+
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .cookie(ACCESS_TOKEN)
+                .body(postMemberInfoReqDto)
+                .when().put("/members")
+                .then().log().all()
+                .assertThat()
+                .apply(document("members/put-ssafy-information",
+                        requestCookieAccessTokenMandatory(),
+                        getEnvelopPatternWithData()
+                                .and(fieldWithPath("data.memberId").type(JsonFieldType.NUMBER)
+                                        .description("멤버 ID"))
+                                .and(fieldWithPath("data.memberRole").type(JsonFieldType.STRING)
+                                        .description("멤버 권한"))
+                                .and(fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                        .description("닉네임"))
+                                .and(fieldWithPath("data.ssafyMember").type(JsonFieldType.BOOLEAN)
+                                        .description("싸피인 여부 FALSE"))
+                                .and(fieldWithPath("data.isMajor").type(JsonFieldType.BOOLEAN)
+                                        .description("전공자 여부 TRUE OR FALSE"))
+                                .and(fieldWithPath("data.ssafyInfo").type(JsonFieldType.OBJECT)
+                                        .description("싸피인 정보"))
+                                .andWithPrefix("data.ssafyInfo.",
+                                        fieldWithPath("semester").type(JsonFieldType.NUMBER)
+                                                .description("싸피 기수 정보"),
+                                        fieldWithPath("campus").type(JsonFieldType.STRING)
+                                                .description("캠퍼스 이름"),
+                                        fieldWithPath("certificationState").type(JsonFieldType.STRING)
+                                                .description("싸피생 인증상태(CERTIFIED/UNCERTIFIED)"),
+                                        fieldWithPath("majorTrack").type(JsonFieldType.NULL)
                                                 .description("전공 트랙(인증상태가 UNCERTIFIED라면 NULL)"))))
                 .expect(status().isOk());
     }
