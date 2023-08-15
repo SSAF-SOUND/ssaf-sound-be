796ac21b318ca7c97492f4dadbee0dc20519f618
김태용 <amico741@naver.com>
2023-08-15 16:36:30 +0900

    feat: Member의 회원가입 플로우 Fixture 추가

--- src/test/java/com/ssafy/ssafsound/global/util/fixture/MemberFixture.java
+++ src/test/java/com/ssafy/ssafsound/global/util/fixture/MemberFixture.java
@@ -1,15 +1,40 @@
 package com.ssafy.ssafsound.global.util.fixture;
 
-import com.ssafy.ssafsound.domain.member.domain.*;
-import com.ssafy.ssafsound.domain.meta.domain.Campus;
-import com.ssafy.ssafsound.domain.meta.domain.MajorTrack;
+import com.ssafy.ssafsound.domain.member.dto.SSAFYInfo;
 import com.ssafy.ssafsound.domain.meta.domain.MetaData;
+import com.ssafy.ssafsound.domain.meta.domain.MajorTrack;
+import com.ssafy.ssafsound.domain.meta.domain.Campus;
+import com.ssafy.ssafsound.domain.member.domain.*;
 
 import java.time.LocalDateTime;
 
 public class MemberFixture {
 
     private static final MemberRole memberRole = MemberRole.builder().id(1).roleType("user").build();
+
+    public static final Member INITIALIZER_MEMBER = Member.builder()
+            .id(1L)
+            .role(memberRole)
+            .build();
+
+    public static final Member GENERAL_MEMBER = Member.builder()
+            .id(1L)
+            .role(memberRole)
+            .nickname("james")
+            .ssafyMember(false)
+            .major(true)
+            .build();
+
+    public static final Member SSAFY_MEMBER = Member.builder()
+            .id(1L)
+            .role(memberRole)
+            .nickname("paul")
+            .ssafyMember(true)
+            .major(false)
+            .build();
+
+    public static final SSAFYInfo SSAFY_INFO = SSAFYInfo
+            .of(9, "서울", "CERTIFIED", new MetaData(MajorTrack.JAVA));
 
     public static final Member MEMBER_WALTER = Member.builder()
             .id(1L)
