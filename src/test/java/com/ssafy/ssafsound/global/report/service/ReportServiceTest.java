5a46bcc98b04060536fa554d04b1100030a431a1
moonn6pence <shin421179@gmail.com>
2023-08-27 19:23:23 +0900

    refactor : fixture를 활용하도록 테스트코드 리팩토링

--- src/test/java/com/ssafy/ssafsound/global/report/service/ReportServiceTest.java
+++ src/test/java/com/ssafy/ssafsound/global/report/service/ReportServiceTest.java
@@ -1,21 +1,15 @@
 package com.ssafy.ssafsound.global.report.service;
 
 import com.ssafy.ssafsound.domain.chat.repository.ChatRoomRepository;
-import com.ssafy.ssafsound.domain.comment.domain.Comment;
 import com.ssafy.ssafsound.domain.comment.repository.CommentRepository;
-import com.ssafy.ssafsound.domain.member.domain.Member;
 import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
 import com.ssafy.ssafsound.domain.meta.domain.MetaData;
 import com.ssafy.ssafsound.domain.meta.domain.MetaDataType;
 import com.ssafy.ssafsound.domain.meta.domain.SourceType;
 import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
-import com.ssafy.ssafsound.domain.post.domain.Post;
 import com.ssafy.ssafsound.domain.post.repository.PostRepository;
-import com.ssafy.ssafsound.domain.recruit.domain.Recruit;
 import com.ssafy.ssafsound.domain.recruit.repository.RecruitRepository;
-import com.ssafy.ssafsound.domain.recruitcomment.domain.RecruitComment;
 import com.ssafy.ssafsound.domain.recruitcomment.repository.RecruitCommentRepository;
-import com.ssafy.ssafsound.global.report.domain.ReportReason;
 import com.ssafy.ssafsound.global.report.dto.PostReportReqDto;
 import com.ssafy.ssafsound.global.report.exception.ReportErrorInfo;
 import com.ssafy.ssafsound.global.report.exception.ReportException;
@@ -33,8 +27,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static com.ssafy.ssafsound.global.util.fixture.CommentFixture.COMMENT_FIXTURE_1;
+import static com.ssafy.ssafsound.global.util.fixture.MemberFixture.MEMBER_SHERYL;
+import static com.ssafy.ssafsound.global.util.fixture.MemberFixture.MEMBER_WALTER;
+import static com.ssafy.ssafsound.global.util.fixture.PostFixture.POST_FIXTURE1;
+import static com.ssafy.ssafsound.global.util.fixture.RecruitCommentFixture.RECRUIT_COMMENT_1;
+import static com.ssafy.ssafsound.global.util.fixture.RecruitFixture.RECRUIT_1;
+import static com.ssafy.ssafsound.global.util.fixture.ReportFixture.REPORT_FIXTURE1;
+import static com.ssafy.ssafsound.global.util.fixture.ReportFixture.REPORT_REASON_FIXTURE1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
@@ -64,97 +67,58 @@ class ReportServiceTest {
     @InjectMocks
     private ReportService reportService;
 
-    private Member testMember1;
-
-    private Member testMember2;
-    private Post testPost1;
-
-    private Post testPost2;
-
-    private Recruit testRecruit;
-
-    private Comment testComment;
-
-    private RecruitComment testRecruitComment;
-
-    private final ReportReason reportReason1 = ReportReason.builder()
-            .id(1L)
-            .reason("게시판 성격에 부적절함")
-            .build();
-
-    private final ReportReason reportReason4 = ReportReason.builder()
-            .id(4L)
-            .reason("상업적 광고 및 판매")
-            .build();
+    private final MetaData sourceTypePost = new MetaData(SourceType.POST);
+    private final MetaData sourceTypeRecruit = new MetaData(SourceType.RECRUIT);
+    private final MetaData sourceTypeRecruitComment = new MetaData(SourceType.RECRUIT_COMMENT);
+    private final MetaData sourceTypeComment = new MetaData(SourceType.COMMENT);
+    private final MetaData sourceTypeChat = new MetaData(SourceType.CHAT);
 
     @BeforeEach
     void setUp() {
 
-        testMember1 = Member.builder()
-                .id(1L)
-                .build();
+        lenient().when(memberRepository.findById(POST_FIXTURE1.getMember().getId()))
+                .thenReturn(Optional.of(POST_FIXTURE1.getMember()));
+        lenient().when(memberRepository.findById(RECRUIT_1.getMember().getId()))
+                .thenReturn(Optional.of(RECRUIT_1.getMember()));
+        lenient().when(memberRepository.findById(COMMENT_FIXTURE_1.getMember().getId()))
+                .thenReturn(Optional.of(COMMENT_FIXTURE_1.getMember()));
+        lenient().when(memberRepository.findById(RECRUIT_COMMENT_1.getMember().getId()))
+                .thenReturn(Optional.of(RECRUIT_COMMENT_1.getMember()));
 
-        testMember2 = Member.builder()
-                .id(2L)
-                .build();
-
-        testPost1 = Post.builder()
-                .id(1L)
-                .member(testMember1)
-                .build();
-
-        testPost2 = Post.builder()
-                .id(1L)
-                .member(testMember2)
-                .build();
-
-        testRecruit = Recruit.builder()
-                .id(3L)
-                .member(testMember2)
-                .build();
-
-        testComment = Comment.builder()
-                .id(1L)
-                .member(testMember2)
-                .build();
-
-        testRecruitComment = RecruitComment.builder()
-                .id(1L)
-                .member(testMember2)
-                .build();
-
-        lenient().when(memberRepository.findById(1L)).thenReturn(Optional.of(testMember1));
-        lenient().when(memberRepository.findById(2L)).thenReturn(Optional.of(testMember2));
-
-        lenient().when(postRepository.findByIdRegardlessOfDeleted(1L)).thenReturn(Optional.of(testPost1));
-        lenient().when(postRepository.findByIdRegardlessOfDeleted(2L)).thenReturn(Optional.of(testPost2));
-        lenient().when(recruitRepository.findById(3L)).thenReturn(Optional.of(testRecruit));
-        lenient().when(commentRepository.findById(1L)).thenReturn(Optional.of(testComment));
-        lenient().when(recruitCommentRepository.findById(1L)).thenReturn(Optional.of(testRecruitComment));
+        lenient().when(postRepository.findByIdRegardlessOfDeleted(POST_FIXTURE1.getId()))
+                .thenReturn(Optional.of(POST_FIXTURE1));
+        lenient().when(recruitRepository.findById(RECRUIT_1.getId())).thenReturn(Optional.of(RECRUIT_1));
+        lenient().when(commentRepository.findById(COMMENT_FIXTURE_1.getId())).thenReturn(Optional.of(COMMENT_FIXTURE_1));
+        lenient().when(recruitCommentRepository.findById(RECRUIT_COMMENT_1.getId()))
+                .thenReturn(Optional.of(RECRUIT_COMMENT_1));
 
         lenient().when(commentRepository.findById(10L)).thenReturn(Optional.empty());
 
         lenient().when(metaDataConsumer.getMetaData(MetaDataType.SOURCE_TYPE.name(), "post"))
-                .thenReturn(new MetaData(SourceType.POST));
+                .thenReturn(sourceTypePost);
         lenient().when(metaDataConsumer.getMetaData(MetaDataType.SOURCE_TYPE.name(), "recruit"))
-                .thenReturn(new MetaData(SourceType.RECRUIT));
+                .thenReturn(sourceTypeRecruit);
         lenient().when(metaDataConsumer.getMetaData(MetaDataType.SOURCE_TYPE.name(), "comment"))
-                .thenReturn(new MetaData(SourceType.COMMENT));
+                .thenReturn(sourceTypeComment);
         lenient().when(metaDataConsumer.getMetaData(MetaDataType.SOURCE_TYPE.name(), "recruit_comment"))
-                .thenReturn(new MetaData(SourceType.RECRUIT_COMMENT));
+                .thenReturn(sourceTypeRecruitComment);
         lenient().when(metaDataConsumer.getMetaData(MetaDataType.SOURCE_TYPE.name(), "chat"))
-                .thenReturn(new MetaData(SourceType.CHAT));
+                .thenReturn(sourceTypeChat);
     }
 
     @ParameterizedTest
-    @CsvSource({"post, 2, 1", "recruit, 3, 1", "comment, 1, 1", "recruit_comment, 1, 1"})
+    @CsvSource({"post, 1, 1", "recruit, 1, 1", "comment, 1, 1", "recruit_comment, 1, 1"})
     @DisplayName("컨텐츠 종류, 컨텐츠 PK, 신고 사유 PK로 신고를 요청한다.")
     void Given_ValidPostSourceTypeAndSourceIdAndReasonId_When_PostReport_Then_Succeed(String sourceType, Long sourceId, Long reasonId) {
 
         // given
-        Long reportMemberId = 1L;
+        Long reportMemberId = MEMBER_SHERYL.getId();
 
-        given(reportReasonRepository.findById(1L)).willReturn(Optional.of(reportReason1));
+        given(memberRepository.findById(MEMBER_SHERYL.getId())).willReturn(Optional.of(MEMBER_SHERYL));
+        given(reportReasonRepository.findById(REPORT_REASON_FIXTURE1.getId()))
+                .willReturn(Optional.of(REPORT_REASON_FIXTURE1));
+        given(reportRepository.existsBySourceTypeAndSourceIdAndReportMemberId(any(), any(), any()))
+                .willReturn(Boolean.FALSE);
 
         PostReportReqDto postReportReqDto = PostReportReqDto.builder()
                 .sourceType(sourceType)
@@ -166,7 +130,7 @@ class ReportServiceTest {
         reportService.postReport(reportMemberId, postReportReqDto);
 
         // then
-        verify(reportReasonRepository).findById(1L);
+        verify(reportReasonRepository).findById(REPORT_REASON_FIXTURE1.getId());
 
     }
 
@@ -175,13 +139,17 @@ class ReportServiceTest {
     void Given_InvalidSourceType_When_PostReport_Then_ThrowException() {
 
         // given
-        Long reportMemberId = 1L;
+        Long reportMemberId = POST_FIXTURE1.getMember().getId();
 
-        String sourceType = "post";
-        Long sourceId = 1L;
-        Long reasonId = 1L;
+        String sourceType = sourceTypePost.getName();
+        Long sourceId = POST_FIXTURE1.getId();
+        Long reasonId = REPORT_REASON_FIXTURE1.getId();
 
-        given(reportReasonRepository.findById(1L)).willReturn(Optional.of(reportReason1));
+        given(memberRepository.findById(reportMemberId)).willReturn(Optional.of(POST_FIXTURE1.getMember()));
+        given(reportReasonRepository.findById(REPORT_REASON_FIXTURE1.getId()))
+                .willReturn(Optional.of(REPORT_REASON_FIXTURE1));
+        given(reportRepository.existsBySourceTypeAndSourceIdAndReportMemberId(sourceTypePost, sourceId, reportMemberId))
+                .willReturn(Boolean.FALSE);
 
         PostReportReqDto postReportReqDto = PostReportReqDto.builder()
                 .sourceType(sourceType)
@@ -194,8 +162,10 @@ class ReportServiceTest {
                 () -> reportService.postReport(reportMemberId, postReportReqDto));
         assertEquals(exception.getInfo(), ReportErrorInfo.UNABLE_SELF_REPORT);
 
-        verify(metaDataConsumer).getMetaData(MetaDataType.SOURCE_TYPE.name(), "post");
-        verify(reportReasonRepository).findById(1L);
+        verify(memberRepository).findById(reportMemberId);
+        verify(metaDataConsumer).getMetaData(MetaDataType.SOURCE_TYPE.name(), sourceTypePost.getName());
+        verify(reportReasonRepository).findById(REPORT_REASON_FIXTURE1.getId());
+        verify(reportRepository).existsBySourceTypeAndSourceIdAndReportMemberId(sourceTypePost, sourceId, reportMemberId);
     }
 
     @Test
@@ -203,13 +173,14 @@ class ReportServiceTest {
     void Given_InvalidReportReasonId_When_PostReport_Then_ThrowException() {
 
         // given
-        Long reportMemberId = 1L;
+        Long reportMemberId = MEMBER_SHERYL.getId();
 
-        String sourceType = "post";
-        Long sourceId = 2L;
+        String sourceType = sourceTypePost.getName();
+        Long sourceId = POST_FIXTURE1.getId();
         Long reasonId = 999L;
 
-        given(reportReasonRepository.findById(999L)).willReturn(Optional.empty());
+        given(memberRepository.findById(MEMBER_SHERYL.getId())).willReturn(Optional.of(MEMBER_SHERYL));
+        given(reportReasonRepository.findById(reasonId)).willReturn(Optional.empty());
 
         PostReportReqDto postReportReqDto = PostReportReqDto.builder()
                 .sourceType(sourceType)
@@ -221,5 +192,32 @@ class ReportServiceTest {
         ReportException exception = assertThrows(ReportException.class,
                 () -> reportService.postReport(reportMemberId, postReportReqDto));
         assertEquals(exception.getInfo(), ReportErrorInfo.INVALID_REPORT_REASON);
+    }
+
+    @Test
+    @DisplayName("이미 신고한 컨텐츠로 신고 요청할 시에 예외를 던진다.")
+    void Given_DuplicatedReport_When_PostReport_Then_ThrowException() {
+
+        // given
+        Long reportMemberId = MEMBER_WALTER.getId();
+
+        String sourceType = sourceTypePost.getName();
+        Long sourceId = REPORT_FIXTURE1.getSourceId();
+        Long reasonId = REPORT_REASON_FIXTURE1.getId();
+
+        given(memberRepository.findById(MEMBER_WALTER.getId())).willReturn(Optional.of(MEMBER_WALTER));
+        given(reportRepository.existsBySourceTypeAndSourceIdAndReportMemberId(sourceTypePost, sourceId, reportMemberId))
+                .willReturn(Boolean.TRUE);
+
+        PostReportReqDto postReportReqDto = PostReportReqDto.builder()
+                .sourceType(sourceType)
+                .sourceId(sourceId)
+                .reasonId(reasonId)
+                .build();
+
+        // when, then
+        ReportException exception = assertThrows(ReportException.class,
+                () -> reportService.postReport(reportMemberId, postReportReqDto));
+        assertEquals(exception.getInfo(), ReportErrorInfo.DUPLICATE_REPORT);
     }
 }
