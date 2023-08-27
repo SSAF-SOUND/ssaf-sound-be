098eac05a33b8acc40c3b65ef575dbc0b984b1b1
khs960616 <anjfkrhdkdle@naver.com>
2023-08-27 20:01:32 +0900

    refacotr: 리크루트 참여 정보 조회 map으로 수정

--- src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/GetRecruitApplicationsResDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/GetRecruitApplicationsResDto.java
@@ -3,22 +3,33 @@ package com.ssafy.ssafsound.domain.recruitapplication.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @NoArgsConstructor
 public class GetRecruitApplicationsResDto {
 
-    private List<RecruitApplicationElement> recruitApplications;
+    private Long recruitId;
+    private Map<String, List<RecruitApplicationElement>> recruitApplications;
 
-    public GetRecruitApplicationsResDto(List<RecruitApplicationElement> recruitApplications) {
-        this.recruitApplications = recruitApplications;
-
+    public GetRecruitApplicationsResDto(Long recruitId, List<RecruitApplicationElement> recruitApplications) {
+        this.recruitId = recruitId;
+        this.recruitApplications = new HashMap<>();
         recruitApplications.sort((r1, r2)->{
-            if(r1.getIsLike().equals(r2.getIsLike())) {
+            if(r1.getLiked().equals(r2.getLiked())) {
                 return r1.getRecruitApplicationId().compareTo(r2.getRecruitApplicationId());
             }
-            return Boolean.compare(r2.getIsLike(), r1.getIsLike());
+            return Boolean.compare(r2.getLiked(), r1.getLiked());
+        });
+
+        recruitApplications.forEach(recruitApplicationElement -> {
+            List<RecruitApplicationElement> applications = this.recruitApplications
+                    .getOrDefault(recruitApplicationElement.getRecruitType(), new ArrayList<>());
+            applications.add(recruitApplicationElement);
+            this.recruitApplications.put(recruitApplicationElement.getRecruitType(), applications);
         });
     }
 }
