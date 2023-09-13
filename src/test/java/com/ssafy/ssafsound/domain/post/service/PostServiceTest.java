133596f27c423e3df2aeb408e4a72bcf2691b39e
jjuny7712 <jjuny7712@gmail.com>
2023-09-13 20:50:04 +0900

    test: 로그인 시 유효한 postId와 loginMemberId가 주어졌다면 게시글 상세 보기가 성공합니다.

--- src/test/java/com/ssafy/ssafsound/domain/post/service/PostServiceTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/post/service/PostServiceTest.java
@@ -1,14 +1,13 @@
 package com.ssafy.ssafsound.domain.post.service;
 
-import com.ssafy.ssafsound.domain.board.exception.BoardErrorInfo;
-import com.ssafy.ssafsound.domain.board.exception.BoardException;
-import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
-import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
-import com.ssafy.ssafsound.domain.post.domain.Post;
-import com.ssafy.ssafsound.domain.post.dto.GetPostReqDto;
-import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
-import com.ssafy.ssafsound.domain.post.repository.*;
-import com.ssafy.ssafsound.infra.storage.service.AwsS3StorageService;
+import static com.ssafy.ssafsound.global.util.fixture.PostFixture.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,141 +15,207 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
-import static com.ssafy.ssafsound.global.util.fixture.PostFixture.POST_FIXTURE1;
-import static com.ssafy.ssafsound.global.util.fixture.PostFixture.POST_FIXTURE2;
-import static org.mockito.BDDMockito.given;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import com.ssafy.ssafsound.domain.board.exception.BoardErrorInfo;
+import com.ssafy.ssafsound.domain.board.exception.BoardException;
+import com.ssafy.ssafsound.domain.board.repository.BoardRepository;
+import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.member.repository.MemberRepository;
+import com.ssafy.ssafsound.domain.post.domain.Post;
+import com.ssafy.ssafsound.domain.post.dto.GetPostDetailResDto;
+import com.ssafy.ssafsound.domain.post.dto.GetPostReqDto;
+import com.ssafy.ssafsound.domain.post.dto.GetPostResDto;
+import com.ssafy.ssafsound.domain.post.repository.HotPostRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostImageRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostLikeRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostRepository;
+import com.ssafy.ssafsound.domain.post.repository.PostScrapRepository;
+import com.ssafy.ssafsound.global.util.fixture.MemberFixture;
+import com.ssafy.ssafsound.infra.storage.service.AwsS3StorageService;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
-    @Mock
-    private BoardRepository boardRepository;
+	@Mock
+	private BoardRepository boardRepository;
 
-    @Mock
-    private MemberRepository memberRepository;
+	@Mock
+	private MemberRepository memberRepository;
 
-    @Mock
-    private PostRepository postRepository;
+	@Mock
+	private PostRepository postRepository;
 
-    @Mock
-    private PostLikeRepository postLikeRepository;
+	@Mock
+	private PostLikeRepository postLikeRepository;
 
-    @Mock
-    private HotPostRepository hotPostRepository;
+	@Mock
+	private HotPostRepository hotPostRepository;
 
-    @Mock
-    private PostScrapRepository postScrapRepository;
+	@Mock
+	private PostScrapRepository postScrapRepository;
 
-    @Mock
-    private PostImageRepository postImageRepository;
+	@Mock
+	private PostImageRepository postImageRepository;
 
-    @Mock
-    private AwsS3StorageService awsS3StorageService;
+	@Mock
+	private AwsS3StorageService awsS3StorageService;
 
-    @Mock
-    private PostConstantProvider postConstantProvider;
+	@Mock
+	private PostConstantProvider postConstantProvider;
 
-    @InjectMocks
-    private PostService postService;
+	@InjectMocks
+	private PostService postService;
 
+	@Test
+	@DisplayName("유효한 boardId, cursor, size가 주어졌다면 게시글 목록 조회가 성공합니다.")
+	void Given_BoardIdAndCursorAndSize_When_findPosts_Then_Success() {
+		// given
+		GetPostReqDto getPostReqDto = GetPostReqDto.builder()
+			.boardId(1L)
+			.cursor(-1L)
+			.size(10)
+			.build();
 
-    @Test
-    @DisplayName("정상적인 boardId, cursor, size가 주어졌다면 게시글 목록 조회가 성공합니다.")
-    void Given_BoardIdAndCursorAndSize_When_findPosts_Then_Success() {
-        // given
-        GetPostReqDto getPostReqDto = GetPostReqDto.builder()
-                .boardId(1L)
-                .cursor(-1L)
-                .size(10)
-                .build();
+		List<Post> posts = List.of(POST_FIXTURE1, POST_FIXTURE2);
 
-        List<Post> posts = List.of(POST_FIXTURE1, POST_FIXTURE2);
+		given(boardRepository.existsById(getPostReqDto.getBoardId())).willReturn(true);
+		given(postRepository.findWithDetailsByBoardId(getPostReqDto.getBoardId(), getPostReqDto.getCursor(),
+			getPostReqDto.getSize())).willReturn(posts);
 
-        given(boardRepository.existsById(getPostReqDto.getBoardId())).willReturn(true);
-        given(postRepository.findWithDetailsByBoardId(getPostReqDto.getBoardId(), getPostReqDto.getCursor(), getPostReqDto.getSize())).willReturn(posts);
+		// when
+		GetPostResDto response = postService.findPosts(getPostReqDto);
 
-        // when
-        GetPostResDto response = postService.findPosts(getPostReqDto);
+		// then
+		assertThat(response).usingRecursiveComparison()
+			.isEqualTo(GetPostResDto.ofPosts(posts, getPostReqDto.getSize()));
 
-        // then
-        assertThat(response).usingRecursiveComparison().isEqualTo(GetPostResDto.ofPosts(posts, getPostReqDto.getSize()));
+		// verify
+		verify(boardRepository, times(1)).existsById(getPostReqDto.getBoardId());
+		verify(postRepository, times(1)).findWithDetailsByBoardId(getPostReqDto.getBoardId(), getPostReqDto.getCursor(),
+			getPostReqDto.getSize());
+	}
 
-        // verify
-        verify(boardRepository, times(1)).existsById(getPostReqDto.getBoardId());
-        verify(postRepository, times(1)).findWithDetailsByBoardId(getPostReqDto.getBoardId(), getPostReqDto.getCursor(), getPostReqDto.getSize());
-    }
+	@Test
+	@DisplayName("유효하지 않은 boardId가 주어졌다면 게시글 목록 조회에 예외를 발생합니다.")
+	void Given_BoardId_When_findPosts_Then_Fail() {
+		// given
+		GetPostReqDto getPostReqDto = GetPostReqDto.builder()
+			.boardId(100L)
+			.cursor(-1L)
+			.size(10)
+			.build();
 
-    @Test
-    @DisplayName("존재하지 않은 boardId가 주어졌다면 게시글 목록 조회가 실패합니다.")
-    void Given_BoardId_When_findPosts_Then_Fail() {
-        // given
-        GetPostReqDto getPostReqDto = GetPostReqDto.builder()
-                .boardId(100L)
-                .cursor(-1L)
-                .size(10)
-                .build();
+		given(boardRepository.existsById(getPostReqDto.getBoardId())).willReturn(false);
 
-        given(boardRepository.existsById(getPostReqDto.getBoardId())).willReturn(false);
+		// when, then
+		BoardException exception = assertThrows(BoardException.class, () -> postService.findPosts(getPostReqDto));
+		assertEquals(BoardErrorInfo.NO_BOARD, exception.getInfo());
 
-        // when, then
-        BoardException exception = assertThrows(BoardException.class, () -> postService.findPosts(getPostReqDto));
-        assertEquals(BoardErrorInfo.NO_BOARD, exception.getInfo());
+		// verify
+		verify(boardRepository, times(1)).existsById(getPostReqDto.getBoardId());
+	}
 
-        // verify
-        verify(boardRepository, times(1)).existsById(getPostReqDto.getBoardId());
-    }
+	@Test
+	@DisplayName("로그인 시 유효한 postId와 loginMemberId가 주어졌다면 게시글 상세 보기가 성공합니다.")
+	void Given_PostIdAndLoginMemberId_When_findPost_Then_Success() {
+		// given
+		Post post = POST_FIXTURE1;
+		Member member = MemberFixture.GENERAL_MEMBER;
 
-    @Test
-    void findPost() {
-    }
+		given(postRepository.findWithMemberAndPostImageFetchById(post.getId())).willReturn(Optional.of(post));
+		given(memberRepository.findById(member.getId())).willReturn(Optional.of(member));
 
-    @Test
-    void likePost() {
-    }
+		// when
+		GetPostDetailResDto response = postService.findPost(post.getId(), member.getId());
 
-    @Test
-    void deleteHotPostsUnderThreshold() {
-    }
+		// then
+		assertThat(response).usingRecursiveComparison()
+			.isEqualTo(GetPostDetailResDto.of(post, member));
 
-    @Test
-    void scrapPost() {
-    }
+		// verify
+		verify(postRepository, times(1)).findWithMemberAndPostImageFetchById(post.getId());
+		verify(memberRepository, times(1)).findById(member.getId());
+	}
 
-    @Test
-    void writePost() {
-    }
+	@Test
+	@DisplayName("비 로그인 시 유효한 postId가 주어졌다면 게시글 상세 보기가 성공합니다.")
+	void Given_PostId_When_findPost_Then_Success() {
+		// given
 
-    @Test
-    void deletePost() {
-    }
+		// when
 
-    @Test
-    void updatePost() {
-    }
+		// then
 
-    @Test
-    void findHotPosts() {
-    }
+		// verify
 
-    @Test
-    void findMyPosts() {
-    }
+	}
 
-    @Test
-    void findMyScrapPosts() {
-    }
+	@Test
+	@DisplayName("유효하지 않은 postId가 주어졌다면 게시글 상세보기에 예외를 발생합니다.")
+	void Given_InvalidPostId_When_findPost_Then_Success() {
+		// given
 
-    @Test
-    void searchPosts() {
-    }
+		// when
 
-    @Test
-    void searchHotPosts() {
-    }
+		// then
+
+		// verify
+
+	}
+
+	@Test
+	@DisplayName("로그인 시 유효하지 않은 loginMemberId가 주어졌다면 게시글 상세보기에 예외를 발생합니다.")
+	void Given_InvalidLoginMemberId_When_findPost_Then_Success() {
+		// given
+
+		// when
+
+		// then
+
+		// verify
+
+	}
+
+	@Test
+	void likePost() {
+	}
+
+	@Test
+	void deleteHotPostsUnderThreshold() {
+	}
+
+	@Test
+	void scrapPost() {
+	}
+
+	@Test
+	void writePost() {
+	}
+
+	@Test
+	void deletePost() {
+	}
+
+	@Test
+	void updatePost() {
+	}
+
+	@Test
+	void findHotPosts() {
+	}
+
+	@Test
+	void findMyPosts() {
+	}
+
+	@Test
+	void findMyScrapPosts() {
+	}
+
+	@Test
+	void searchPosts() {
+	}
+
+	@Test
+	void searchHotPosts() {
+	}
 }
