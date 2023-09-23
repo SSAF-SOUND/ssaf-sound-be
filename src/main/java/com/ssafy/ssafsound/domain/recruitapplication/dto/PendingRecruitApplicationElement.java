22a20cbda535dedfa43425b89b6709b42c994f18
khs960616 <55014803+khs960616@users.noreply.github.com>
2023-09-23 13:58:57 +0900

    refactor: 리크루트 신청 신청시간, 확정시간 필드명 수정
    * refactor: 리크루트 참여자 목록 조회 - 참여 확정일 필드명 수정
    
    * fix: 리크루트 신청 신청일 누락 추가
    
    * fix: 등록자 리크루트 신청 목록 조회
    
    * fix: 등록자 리쿠르트 참여 조회 및 docs 날짜관련 필드 설명 수정

--- src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/PendingRecruitApplicationElement.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruitapplication/dto/PendingRecruitApplicationElement.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.recruitapplication.dto;
 
 import com.ssafy.ssafsound.domain.member.dto.AuthorElement;
+import com.ssafy.ssafsound.domain.recruitapplication.domain.MatchStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,26 +9,22 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-public class GetRecruitApplicationDetailResDto {
-    private Long recruitId;
+public class PendingRecruitApplicationElement {
     private Long recruitApplicationId;
-    private String recruitType;
-    private String matchStatus;
+    private MatchStatus matchStatus;
     private AuthorElement author;
     private String reply;
     private String question;
     private Boolean liked;
     private LocalDateTime appliedAt;
 
-    public GetRecruitApplicationDetailResDto(RecruitApplicationElement recruitApplicationElement) {
-        this.recruitId = recruitApplicationElement.getRecruitId();
+    public PendingRecruitApplicationElement(RecruitApplicationElement recruitApplicationElement) {
         this.recruitApplicationId = recruitApplicationElement.getRecruitApplicationId();
-        this.recruitType = recruitApplicationElement.getRecruitType();
-        this.matchStatus = recruitApplicationElement.getMatchStatus().name();
-        this.author = recruitApplicationElement.getAuthor();
+        this.matchStatus =  recruitApplicationElement.getMatchStatus();
+        this.author =  recruitApplicationElement.getAuthor();
         this.reply = recruitApplicationElement.getReply();
-        this.question = recruitApplicationElement.getQuestion();
-        this.liked = recruitApplicationElement.getLiked();
+        this.question =  recruitApplicationElement.getQuestion();
+        this.liked =  recruitApplicationElement.getLiked();
         this.appliedAt = recruitApplicationElement.getCreatedAt();
     }
 }
