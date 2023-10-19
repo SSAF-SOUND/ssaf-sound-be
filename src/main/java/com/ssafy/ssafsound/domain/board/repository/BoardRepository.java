334231731402c5c17de549233e0f8bc7fbc6d249
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-10-19 16:51:58 +0900

    test: 게시글 쓰기 단위 테스트 코드 구현
    * refactor: Post 관련 테스트 메소드 네이밍 수정
    
    * fix: boardFixture 메소드로 가져오도록 수정 및 usedBoard 필드 추가
    
    * fix: 게시판 조회 쿼리문 수정
    
    * refactor: 엔티티 생성하는 메소드 내부로 분리
    
    * test: 정상적인 boardId, MemberId가 주어졌다면 게시글 쓰기가 성공하는 테스트 코드 작성
    
    * test: 정상적인 boardId, MemberId가 주어졌다면 게시글 쓰기가 성공합니다.(이미지 O)
    
    * test: 유효하지 않은 boardID가 주어졌다면 게시글 쓰기에 예외를 발생하는 테스트 코드 구현
    
    * test: 유효하지 않은 memberId가 주어졌다면 게시글 쓰기에 예외를 발생하는 테스트 코드 구현

--- src/main/java/com/ssafy/ssafsound/domain/board/repository/BoardRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/board/repository/BoardRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.ssafsound.domain.board.repository;
 
+import java.util.List;
+
 import com.ssafy.ssafsound.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+	List<Board> findAllByUsedBoardTrue();
 }
--- src/test/java/com/ssafy/ssafsound/domain/board/controller/BoardControllerTest.java
+++ src/test/java/com/ssafy/ssafsound/domain/board/controller/BoardControllerTest.java
@@ -1,23 +1,32 @@
 package com.ssafy.ssafsound.domain.board.controller;
 
+import com.ssafy.ssafsound.domain.board.domain.Board;
+import com.ssafy.ssafsound.domain.board.dto.GetBoardResDto;
 import com.ssafy.ssafsound.global.docs.ControllerTest;
+import com.ssafy.ssafsound.global.util.fixture.BoardFixture;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import static com.ssafy.ssafsound.global.docs.snippet.CookieDescriptionSnippet.requestCookieAccessTokenNeedless;
-import static com.ssafy.ssafsound.global.docs.snippet.CookieDescriptionSnippet.requestCookieAccessTokenOptional;
-import static com.ssafy.ssafsound.global.util.fixture.BoardFixture.GET_BOARD_RES_DTO1;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
+import java.util.List;
+
 class BoardControllerTest extends ControllerTest {
+    private final BoardFixture boardFixture = new BoardFixture();
+
     @Test
     @DisplayName("게시판 목록 조회 데이터 반환")
     public void findBoards() {
-        doReturn(GET_BOARD_RES_DTO1)
+        List<Board> boards = List.of(boardFixture.getFreeBoard(), boardFixture.getJobBoard());
+        GetBoardResDto getBoardResDto = GetBoardResDto.from(boards);
+
+        doReturn(getBoardResDto)
                 .when(boardService)
                 .findBoards();
 
@@ -33,6 +42,7 @@ class BoardControllerTest extends ControllerTest {
                                 fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판 목록의 ID"),
                                 fieldWithPath("title").type(JsonFieldType.STRING).description("게시판 목록의 제목, 자유 게시판 | 취업 게시판 | 맛집 게시판 | 질문 게시판 | 싸피 예비생 게시판"),
                                 fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("게시판 배너 이미지"),
+                                fieldWithPath("usedBoard").type(JsonFieldType.BOOLEAN).description("게시판 사용 여부"),
                                 fieldWithPath("description").type(JsonFieldType.STRING).description("게시판의 특성을 설명해주는 설명문")
                         )));
     }
