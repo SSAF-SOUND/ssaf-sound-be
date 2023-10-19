6bf9a3b0eeac30bc10605433689307413d028694
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-10-19 16:52:14 +0900

    fix: 페이지네이션 cursor 버그 수정
    * fix: cursor 디폴트 -1로 설정
    
    * fix: 삭제한 클래스 import 삭제

--- src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostReqDto.java
@@ -1,19 +1,18 @@
 package com.ssafy.ssafsound.domain.post.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.Min;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetPostReqDto {
     private Long boardId;
-    private Long cursor;
+    private Long cursor = -1L;
 
     @Min(value = 10, message = "Size가 너무 작습니다.")
     private int size;
-
 }
