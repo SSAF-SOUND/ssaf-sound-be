0fc757be6b5fc4b128578613a765776ec6df3ccd
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-10-19 21:19:36 +0900

    fix: 페이지네이션 cursor 버그 수정
    * refactor: ModelAttribute 명시
    
    * fix: cursor 디폴트 -1로 수정

--- src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostMyReqDto.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/dto/GetPostMyReqDto.java
@@ -1,14 +1,16 @@
 package com.ssafy.ssafsound.domain.post.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.Min;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetPostMyReqDto {
-    private Long cursor;
+    private Long cursor = -1L;
 
     @Min(value = 10, message = "Size가 너무 작습니다.")
     private int size;
