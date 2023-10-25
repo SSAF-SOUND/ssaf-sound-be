9df094c545594f4ff323c29a76603b8129e6f412
khs960616 <anjfkrhdkdle@naver.com>
2023-10-26 00:27:23 +0900

    fix: 매칭 완료된 리크루트 존재시, current number 일괄 변경 추가

--- src/main/java/com/ssafy/ssafsound/domain/recruit/event/RecruitEventListener.java
+++ src/main/java/com/ssafy/ssafsound/domain/recruit/event/RecruitEventListener.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssafsound.domain.recruit.event;
 
 import com.ssafy.ssafsound.domain.event.MemberLeavedEvent;
+import com.ssafy.ssafsound.domain.recruit.repository.RecruitLimitationRepository;
 import com.ssafy.ssafsound.domain.recruit.repository.RecruitRepository;
 import com.ssafy.ssafsound.domain.recruitapplication.repository.RecruitApplicationRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class RecruitEventListener {
 
     private final RecruitRepository recruitRepository;
+    private final RecruitLimitationRepository recruitLimitationRepository
     private final RecruitApplicationRepository recruitApplicationrepository;
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
@@ -22,6 +24,7 @@ public class RecruitEventListener {
         Long memberId = memberLeavedEvent.getMemberId();
         log.info("{} 회원 탈퇴로 인한 등록 리크루트글 일괄 삭제 및 리크루트 신청 일괄 취소", memberLeavedEvent.getMemberId());
         recruitRepository.deleteAllByMemberId(memberId);
+        recruitLimitationRepository.decreaseCurrentNumberByMemberId(memberId);
         recruitApplicationrepository.cancelAllByMemberId(memberId);
     }
 }
