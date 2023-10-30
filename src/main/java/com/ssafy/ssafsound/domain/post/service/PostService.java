f691d73f4ae68f1dd9d9db78d09b0e914a0a441f
이용준 <59381113+jjuny0310@users.noreply.github.com>
2023-10-30 13:00:25 +0900

    fix: Hot 게시글 삭제 batch -> scheduler로 수정, 삭제 쿼리 수정

--- src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
+++ src/main/java/com/ssafy/ssafsound/domain/post/service/PostService.java
@@ -20,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
