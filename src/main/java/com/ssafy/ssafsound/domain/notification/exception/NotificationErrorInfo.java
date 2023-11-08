6a74588e433b7c207b7a45247f005f3ff0531e75
jjuny7712 <jjuny7712@gmail.com>
2023-11-08 17:08:03 +0900

    feat: 예외 메시지 및 에러코드 추가

--- src/main/java/com/ssafy/ssafsound/domain/notification/exception/NotificationErrorInfo.java
+++ src/main/java/com/ssafy/ssafsound/domain/notification/exception/NotificationErrorInfo.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum NotificationErrorInfo {
-    NOT_FOUND_SERVICE_TYPE("xxx", "잘못된 서비스 타입입니다."),
-    NOT_FOUND_NOTIFICATION_TYPE("xxx", "잘못된 알림 타입입니다.");
+    NOT_FOUND_SERVICE_TYPE("1200", "잘못된 서비스 타입입니다."),
+    NOT_FOUND_NOTIFICATION_TYPE("1201", "잘못된 알림 타입입니다."),
+    NOT_FOUND_NOTIFICATION_BY_OWNER("1202", "해당 사용자의 알림이 없습니다.");
 
     private final String code;
     private final String message;
