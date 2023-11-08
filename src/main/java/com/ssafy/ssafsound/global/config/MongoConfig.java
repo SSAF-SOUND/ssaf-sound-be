4f852d256be30e64cdc7250750b4bebe96a7dccc
jjuny7712 <jjuny7712@gmail.com>
2023-11-08 17:08:40 +0900

    feat: 몽고DB 관련 config 추가

---
+++ src/main/java/com/ssafy/ssafsound/global/config/MongoConfig.java
@@ -0,0 +1,35 @@
+package com.ssafy.ssafsound.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+@EnableMongoAuditing
+@RequiredArgsConstructor
+public class MongoConfig {
+    private final MongoDatabaseFactory mongoDatabaseFactory;
+    private final MongoMappingContext mongoMappingContext;
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+
+    @Bean
+    public MongoTransactionManager mongoTransactionManager() {
+        return new MongoTransactionManager(mongoDatabaseFactory);
+    }
+
+}
