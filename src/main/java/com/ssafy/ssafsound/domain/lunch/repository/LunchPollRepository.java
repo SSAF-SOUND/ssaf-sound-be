a7315b12357929d1cee5faaad24684b3fbf362b1
신현철 <shin421179@gmail.com>
2023-11-10 14:31:29 +0900

    fix : 점심 투표 정책 변경
    - 다른 캠퍼스여도 중복 일자 투표 불가능했던 점 수정

--- src/main/java/com/ssafy/ssafsound/domain/lunch/repository/LunchPollRepository.java
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/repository/LunchPollRepository.java
@@ -3,8 +3,8 @@ package com.ssafy.ssafsound.domain.lunch.repository;
 import com.ssafy.ssafsound.domain.lunch.domain.Lunch;
 import com.ssafy.ssafsound.domain.lunch.domain.LunchPoll;
 import com.ssafy.ssafsound.domain.member.domain.Member;
+import com.ssafy.ssafsound.domain.meta.domain.MetaData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +14,22 @@ import java.util.Optional;
 @Repository
 public interface LunchPollRepository extends JpaRepository<LunchPoll, Long> {
 
-    LunchPoll findByMemberAndPolledAt(Member member, LocalDate polledAt);
+//    LunchPoll findByMemberAndPolledAt(Member member, LocalDate polledAt);
 
     Optional<LunchPoll> findByMemberAndLunch(Member member, Lunch lunch);
+
+    @Query( "SELECT lp " +
+            "FROM " +
+            "   lunch_poll lp " +
+            "LEFT JOIN " +
+            "   lunch l " +
+            "ON" +
+            "   lp.lunch = l " +
+            "AND" +
+            "   lp.member = :member " +
+            "AND " +
+            "   l.campus = :campus " +
+            "AND " +
+            "   lp.polledAt = :polledAt")
+    LunchPoll findByMemberAndCampusAndPolledAt(Member member, MetaData campus, LocalDate polledAt);
 }
