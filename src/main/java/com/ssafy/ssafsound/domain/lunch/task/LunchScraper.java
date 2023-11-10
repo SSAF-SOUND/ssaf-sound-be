e1fe9f8445987e7b4deaf18992d0a841ae6dabde
신현철 <shin421179@gmail.com>
2023-11-10 17:29:31 +0900

    refactor : 점심 크롤링 리팩토링
    - 스케줄러의 비즈니스 로직을 서비스단으로 분리

--- src/main/java/com/ssafy/ssafsound/domain/lunch/task/LunchScraper.java
+++ src/main/java/com/ssafy/ssafsound/domain/lunch/task/LunchScraper.java
@@ -1,24 +1,13 @@
 package com.ssafy.ssafsound.domain.lunch.task;
 
 
-import com.ssafy.ssafsound.domain.lunch.domain.Lunch;
-import com.ssafy.ssafsound.domain.lunch.repository.LunchRepository;
-import com.ssafy.ssafsound.domain.lunch.task.domain.ScrapInfoProvider;
-import com.ssafy.ssafsound.domain.lunch.task.domain.ScrapInfoProviderFactory;
-import com.ssafy.ssafsound.domain.lunch.task.domain.WelstoryInfoProvider;
-import com.ssafy.ssafsound.domain.lunch.task.dto.GetFreshmealResDto;
-import com.ssafy.ssafsound.domain.lunch.task.dto.GetScrapReqDto;
-import com.ssafy.ssafsound.domain.lunch.task.dto.GetWelstoryResDto;
-import com.ssafy.ssafsound.domain.meta.domain.MetaData;
-import com.ssafy.ssafsound.domain.meta.service.MetaDataConsumer;
+import com.ssafy.ssafsound.domain.lunch.service.LunchScrapService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -26,104 +15,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LunchScraper {
 
-    private final LunchRepository lunchRepository;
-    private final MetaDataConsumer metaDataConsumer;
-    private final ScrapInfoProviderFactory scrapInfoProviderFactory;
+    private final LunchScrapService lunchScrapService;
+    private final List<String> campusNames = List.of("서울", "부울경", "구미");
 
     // 토 ~ 수요일 동안 03:00에 이틀 뒤의 웰스토리 메뉴를 스크래핑
     @Scheduled(cron = "0 0 3 ? * SAT,SUN,MON,TUE,WED")
-    public void scrapWelstory(){
-
-        WelstoryInfoProvider welstoryInfoProvider = (WelstoryInfoProvider) scrapInfoProviderFactory.getProviderFrom("welstory");
-
-        List<Lunch> lunches = new ArrayList<>();
-
-        List<MetaData> campuses = new ArrayList<>();
-        campuses.add(metaDataConsumer.getMetaData("CAMPUS","서울"));
-        campuses.add(metaDataConsumer.getMetaData("CAMPUS","부울경"));
-        campuses.add(metaDataConsumer.getMetaData("CAMPUS","구미"));
+    public void scrapWelstory() {
 
         // 이틀 뒤 메뉴를 가져오기 위한 일자 설정
-        LocalDate menuDt = LocalDate.now().plusDays(2);
+        LocalDate date = LocalDate.now().plusDays(2);
 
-
-        // 로그인
-        welstoryInfoProvider.setSessionHeader();
-
-        for (MetaData campus : campuses) {
-            try {
-                // 캠퍼스 + 날짜로 스크랩 요청을 위한 파라미터 dto 생성
-                GetScrapReqDto request = GetScrapReqDto.builder()
-                        .campus(campus)
-                        .menuDt(menuDt)
-                        .build();
-
-                // 스크랩 해오기
-                GetWelstoryResDto meals = (GetWelstoryResDto) welstoryInfoProvider.scrapLunchInfo(request);
-
-                // 결과 저장
-                meals.getWelstoryBodyData().getMealList().forEach(
-                        meal -> {
-                            Lunch lunch = meal.toEntity(campus);
-
-                            if (lunch != null) {
-                                lunches.add(lunch);
-                            }
-                        }
-                );
-
-            } catch (Exception e) {
-                log.error(e.getMessage());
-                e.printStackTrace();
-
-                throw new RuntimeException(e);
-            }
+        for (String campusName : campusNames) {
+            lunchScrapService.scrapWelstoryLunch(campusName, date);
         }
-
-        // 일괄 저장
-        lunchRepository.saveAll(lunches);
     }
 
     // 매주 토요일 03:02 프레시밀 스크래핑
     @Scheduled(cron = "0 2 3 ? * SAT")
-    public void scrapFreshmeal(){
-        ScrapInfoProvider freshmealInfoProvider = scrapInfoProviderFactory.getProviderFrom("freshmeal");
+    public void scrapFreshmeal() {
 
-        List<Lunch> lunches = new ArrayList<>();
-        // 일주일의 데이터를 한 번에 응답의 요일별 프로퍼티로 받는다.
-        // 요일별 Key
-        List<String> daysOfWeek = Arrays.asList("mo","tu","we","th","fr");
-
-        MetaData campus = metaDataConsumer.getMetaData("CAMPUS","광주");
-
-        for (String day : daysOfWeek) {
-
-            // 캠퍼스 정보로 요청 dto 생성
-            GetScrapReqDto request = GetScrapReqDto.builder()
-                    .campus(campus)
-                    .build();
-
-            // 스크래핑 요청
-            GetFreshmealResDto meals = (GetFreshmealResDto) freshmealInfoProvider.scrapLunchInfo(request);
-
-            // json 형식에 따른 dto를 파싱하고, 리스트를 반복하면서 Lunch 객체를 얻어온다.
-            meals.getData().getDailyMeal().get(day).getMeals().forEach(
-                    meal -> {
-                        try {
-                            Lunch lunch = meal.toEntity(campus);
-                            if (lunch != null) {
-                                lunches.add(lunch);
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-
-                            throw new RuntimeException(e.getMessage());
-                        }
-                    }
-            );
-        }
-
-        // 일괄 저장
-        lunchRepository.saveAll(lunches);
+        lunchScrapService.scrapFreshmealLunch();
     }
 }
